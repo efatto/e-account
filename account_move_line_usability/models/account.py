51c5119c864e8bd91ebedc3b26a356a0f9786736
sergio <sergiocorato@gmail.com>
2017-05-09 23:51:21 +0200

    [IMP] add account move usability

---
+++ account_move_line_usability/models/account.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api, _
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.onchange('credit')
+    def _credit_onchange(self):
+        if self.credit and self.debit:
+            self.debit = 0
+
+    @api.onchange('debit')
+    def _debit_onchange(self):
+        if self.debit and self.credit:
+            self.credit = 0
