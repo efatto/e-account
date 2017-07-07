9b2c25455a77224a1e1a04854f7dc8853fc16696
sergio <sergiocorato@gmail.com>
2017-07-07 10:58:59 +0200

    [IMP] sppp additional product description

--- partner_product_additional_description/models/__init__.py
+++ partner_product_additional_description/models/__init__.py
@@ -5,3 +5,4 @@
 from . import res_partner
 from . import sale_order
 from . import account_invoice
+from . import package_preparation
---
+++ partner_product_additional_description/models/package_preparation.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api
+
+
+class StockPickingPackagePreparation(models.Model):
+    _inherit = 'stock.picking.package.preparation'
+
+    def get_partner_product_additional_description(self):
+        for pack in self:
+            if not pack.partner_id.is_company:
+                pack.partner_product_additional_description_id = \
+                    pack.partner_id.parent_id.\
+                    partner_product_additional_description_id
+            else:
+                pack.partner_product_additional_description_id = \
+                    pack.partner_id.\
+                    partner_product_additional_description_id
+
+    partner_product_additional_description_id = fields.Many2one(
+        comodel_name='product.additional.description',
+        compute=get_partner_product_additional_description,
+    )
