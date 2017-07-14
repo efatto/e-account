8ee52bb3e9b88a6de7471dc046fce0d538398e68
sergio <sergiocorato@gmail.com>
2017-07-14 11:12:04 +0200

    [IMP] compute and refund advance invoice for percentage

---
+++ sale_advance_invoice_progress/models/sale.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields, api, _, exceptions, workflow
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
+import time
+#TODO if order_policy == 'picking' type of invoice creation = 'percentage' or 'fixed' only
+#TODO if order_id.invoice_ids: detrai importi se non sono fatture create dal ddt
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    advance_invoice_id = fields.Many2one('account.invoice', 'Advance invoice')
+
+
+class SaleOrderLineMakeInvoice(models.TransientModel):
+    _inherit = "sale.order.line.make.invoice"
+
+    def make_invoices(self, cr, uid, ids, context=None):
+        if context is None: context = {}
+        res = False
+        invoices = {}
+
+        def make_invoice(order, lines):
+            inv = self._prepare_invoice(cr, uid, order, lines)
+            inv_id = self.pool.get('account.invoice').create(cr, uid, inv)
+            return inv_id
+
+        sales_order_line_obj = self.pool.get('sale.order.line')
+        sales_order_obj = self.pool.get('sale.order')
+        order_invoiced_amount = {}
+        for line in sales_order_line_obj.browse(cr, uid, context.get(
+            'active_ids', []), context=context):
+            if (not line.invoiced) and (line.state not in ('draft', 'cancel')):
+                if not line.order_id in invoices:
+                    invoices[line.order_id] = []
+                line_id = sales_order_line_obj.invoice_line_create(
+                    cr, uid, [line.id])
+                for lid in line_id:
+                    invoices[line.order_id].append(lid)
+                    # TODO get total amount invoiced for order
+                    if not line.order_id.id in order_invoiced_amount:
+                        order_invoiced_amount[line.order_id.id] = \
+                            line.price_subtotal
+                    else:
+                        order_invoiced_amount[line.order_id.id] += \
+                            line.price_subtotal
+        order_invoice = {}
+        for order, il in invoices.items():
+            res = make_invoice(order, il)
+            #
+            if res not in order_invoice:
+                order_invoice[res] = {
+                    order.id: order_invoiced_amount[order.id]}
+            if order.id not in order_invoice[res]:
+                order_invoice[res].update(
+                    {order.id: order_invoiced_amount[order.id]})
+            # else:
+            #     pass # ??? boh non credo possa esserci
+            for advance_invoice in order.advance_invoice_ids:
+                for preline in advance_invoice.invoice_line:
+                    inv_line_id = self.pool['account.invoice.line'].copy(
+                        cr, uid, preline.id, {
+                            'invoice_id': res,
+                            'price_unit': - order_invoice[res][order.id] *
+                            order.advance_percentage / 100 *
+                            preline.price_subtotal / order.advance_amount,
+                            'advance_invoice_id': advance_invoice.id,
+                        })
+            #
+            cr.execute('INSERT INTO sale_order_invoice_rel \
+                    (order_id,invoice_id) values (%s,%s)', (order.id, res))
+            sales_order_obj.invalidate_cache(
+                cr, uid, ['invoice_ids'], [order.id], context=context)
+            flag = True
+            sales_order_obj.message_post(
+                cr, uid, [order.id], body=_("Invoice created"),
+                context=context)
+            data_sale = sales_order_obj.browse(cr, uid, order.id,
+                                               context=context)
+            for line in data_sale.order_line:
+                if not line.invoiced and line.state != 'cancel':
+                    flag = False
+                    break
+            if flag:
+                line.order_id.write({'state': 'progress'})
+                workflow.trg_validate(
+                    uid, 'sale.order', order.id, 'all_lines', cr)
+
+        if not invoices:
+            raise exceptions.ValidationError(
+                _('Invoice cannot be created for this Sales Order Line due to'
+                  ' one of the following reasons:\n1.The state of this sales '
+                  'order line is either "draft" or "cancel"!\n2.The Sales '
+                  'Order Line is Invoiced!'))
+
+        if context.get('open_invoices', False):
+            return self.open_invoices(cr, uid, ids, res, context=context)
+        return {'type': 'ir.actions.act_window_close'}
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _get_advance_invoices(self):
+        order_line_obj = self.env['sale.order.line']
+        for order in self:
+            invoiced_sale_line_ids = order_line_obj.search(
+                [('order_id', '=', order.id), ('invoiced', '=', True)],)
+            order.from_line_invoice_ids = invoiced_sale_line_ids.mapped(
+                'invoice_lines.invoice_id'
+            )
+            order.advance_invoice_ids = order.invoice_ids.filtered(
+                lambda x: x.state not in ('cancel',) and
+                x not in order.from_line_invoice_ids
+            )
+            order.advance_amount = sum(
+                x.amount_untaxed for x in order.advance_invoice_ids)
+            order.advance_percentage = \
+                order.advance_amount / order.amount_untaxed * 100 if \
+                order.advance_amount and order.amount_untaxed else 0.0
+            order.refunded_invoice_line_ids = self.env[
+                'account.invoice.line'].search([
+                    ('advance_invoice_id', 'in', order.advance_invoice_ids.ids)
+                    , ('invoice_id.state', 'in', ['open', 'paid'])
+                ])
+
+    advance_invoice_ids = fields.One2many(
+        comodel_name='account.invoice',
+        compute=_get_advance_invoices,
+        string='Advance Invoices',
+    )
+    from_line_invoice_ids = fields.One2many(
+        comodel_name='account.invoice',
+        compute=_get_advance_invoices,
+        string='Invoices from Order',
+    )
+    refunded_invoice_line_ids = fields.One2many(
+        comodel_name='account.invoice.line',
+        compute=_get_advance_invoices,
+        string='Invoice line advance refunded'
+    )
+    advance_percentage = fields.Float(
+        compute=_get_advance_invoices,
+        string='Advance (%)'
+    )
+    advance_amount = fields.Float(
+        compute=_get_advance_invoices,
+        string='Advance amount'
+    )
+    advance_amount_refunded = fields.Float(
+        compute=_get_advance_invoices,
+        string='Advance amount refunded'
+    )
+
+    @api.cr_uid_context
+    def _make_invoice(self, cr, uid, order, lines, context=None):
+        inv_obj = self.pool.get('account.invoice')
+        obj_invoice_line = self.pool.get('account.invoice.line')
+        if context is None:
+            context = {}
+        invoiced_sale_line_ids = self.pool.get('sale.order.line').search(
+            cr, uid, [('order_id', '=', order.id), ('invoiced', '=', True)],
+            context=context)
+        from_line_invoice_ids = []
+        for invoiced_sale_line_id in self.pool.get('sale.order.line').browse(
+                cr, uid, invoiced_sale_line_ids, context=context):
+            for invoice_line_id in invoiced_sale_line_id.invoice_lines:
+                if invoice_line_id.invoice_id.id not in from_line_invoice_ids:
+                    from_line_invoice_ids.append(invoice_line_id.invoice_id.id)
+        for preinv in order.invoice_ids:
+            if preinv.state not in ('cancel',) and preinv.id not in \
+                    from_line_invoice_ids:
+                for preline in preinv.invoice_line:
+                    inv_line_id = obj_invoice_line.copy(
+                        cr, uid, preline.id, {
+                            'invoice_id': False,
+                            'price_unit': -preline.price_unit,
+                            'advance_invoice_id': preinv.id,
+                        })
+                    lines.append(inv_line_id)
+        inv = self._prepare_invoice(cr, uid, order, lines, context=context)
+        inv_id = inv_obj.create(cr, uid, inv, context=context)
+        data = inv_obj.onchange_payment_term_date_invoice(
+            cr, uid, [inv_id], inv['payment_term'],
+            time.strftime(DEFAULT_SERVER_DATE_FORMAT))
+        if data.get('value', False):
+            inv_obj.write(cr, uid, [inv_id], data['value'], context=context)
+        inv_obj.button_compute(cr, uid, [inv_id])
+        return inv_id
