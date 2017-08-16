005d9c37e8db92a6404064c716d753f3d33c59de
sergio <sergiocorato@gmail.com>
2017-08-16 22:47:50 +0200

    [IMP] discount not apply to other service type products

---
+++ partner_default_sale_discount_service_type/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2017 Sergio Corato
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Default sales discount per partner not for other services',
+    'version': '8.0.1.0.0',
+    'category': 'Sale',
+    'description': 'Discount will not applied to other services types',
+    'license': 'AGPL-3',
+    'author': 'Sergio Corato',
+    'website': 'https://www.efatto.it',
+    'depends': [
+        'partner_default_sale_discount',
+        'service_type',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
---
+++ partner_default_sale_discount_service_type/models/sale_order.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    @api.depends('default_sale_discount', 'order_line')
+    def sale_discount_update(self):
+        for order in self:
+            for line in order.order_line.filtered(
+                lambda x: x.product_id.service_type not in [
+                    'other', 'contribution', 'discount', 'transport']
+            ):
+                line.discount = order.default_sale_discount
