b22820b275785f7b62941def8154aee3f08c083d
sergio <sergiocorato@gmail.com>
2017-08-21 15:11:00 +0200

    [IMP] add generic parser for aeroo

---
+++ report_aeroo_parser/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from . import parser
+from . import account
+from . import company
