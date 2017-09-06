b165e6bc5168ccc1b61b81dd794a5bca98ee92b0
sergio <sergiocorato@gmail.com>
2017-09-06 22:46:31 +0200

    [IMP] add field final destionation for invoice

---
+++ account_invoice_destination/models/account_invoice.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, api, fields, exceptions, _
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    address_destination_id = fields.Many2one(
+        comodel_name="res.partner",
+        string='Final Destination',
+        help="Optional destination for extra CEE.")
