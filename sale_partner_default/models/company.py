4645fdfd88df949b489e2cdca276bc5ec10a9831
sergio <sergiocorato@gmail.com>
2017-09-07 23:02:52 +0200

    [FIX] add default delivery carrier to company

--- sale_partner_default/models/company.py
+++ sale_partner_default/models/company.py
@@ -8,8 +8,12 @@ from openerp import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    sale_partner_default = fields.Many2one(
+    sale_partner_default = fields.Many2one( # this field is unused,delete later
         comodel_name='res.partner',
         company_dependent=True,
         help='Default sale partner for many conditions, as pricelist, '
              'discount, term of payment, etc.')
+    default_delivery_carrier_id = fields.Many2one(
+        comodel_name='delivery.carrier',
+        help='Default delivery carrier.'
+    )
