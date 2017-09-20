ad8f31adc8799c0cb1cc700f9eb9fdae27cb242c
sergio <sergiocorato@gmail.com>
2017-09-20 23:12:29 +0200

    [IMP] replace return-path with from in mail sent

--- mail_outbound_static_company/models/ir_mail_server.py
+++ mail_outbound_static_company/models/ir_mail_server.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 LasLabs Inc.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
 from openerp import api, fields, models
 
 
 class IrMailServer(models.Model):
-
     _inherit = 'ir.mail_server'
 
     @api.model
     def send_email(self, message, mail_server_id=None, smtp_server=None,
                    *args, **kwargs):
 
-        # Replicate logic from core to get mail server
-        mail_server = None
-        if mail_server_id:
-            mail_server = self.sudo().browse(mail_server_id)
-        elif not smtp_server:
-            mail_server = self.sudo().search([], order='sequence', limit=1)
-
-        if mail_server and self.env.user.company_id.email:
-            if not message['Return-Path']:
-                message.add_header('Return-Path', self.env.user.company_id.email)
+        message.add_header('Return-Path', message['From'])
 
         return super(IrMailServer, self).send_email(
             message, mail_server_id, smtp_server, *args, **kwargs
