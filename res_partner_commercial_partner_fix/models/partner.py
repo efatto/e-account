ba1a3172b76b62e77689a65dba0917def8733143
sergio <sergiocorato@gmail.com>
2017-10-01 13:48:13 +0200

    [IMP] invoice commercial partner field not more related - bug when updating is_company field in partner used in more than 1 company

---
+++ res_partner_commercial_partner_fix/models/partner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, api, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    def _get_commercial_partner_id(self):
+        for partner in self.partner_id:
+            current_partner = partner
+            while not current_partner.is_company and current_partner.parent_id:
+                current_partner = current_partner.parent_id
+            self.commercial_partner_id = current_partner
+
+    commercial_partner_id = fields.Many2one(
+        'res.partner', string='Commercial Entity',
+        compute='_get_commercial_partner_id',
+        store=True, readonly=True,
+        help="The commercial entity that will be used on Journal "
+             "Entries for this invoice")
