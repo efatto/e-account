fdb7b6cd773a7c962218160ec8ec9372a3b6d6d1
sergio <sergiocorato@gmail.com>
2017-11-03 10:01:16 +0100

    [FIX] remove unused field from super() of onchange to fix error in change partner opened from open invoice

--- account_invoice_delivery/models/account_invoice.py
+++ account_invoice_delivery/models/account_invoice.py
@@ -18,9 +18,7 @@ class AccountInvoice(models.Model):
             self, type, partner_id, date_invoice=False, payment_term=False,
             partner_bank_id=False, company_id=False):
         res = super(AccountInvoice, self).onchange_partner_id(
-            type, partner_id, date_invoice=date_invoice,
-            payment_term=payment_term, partner_bank_id=partner_bank_id,
-            company_id=company_id)
+            type, partner_id)
         if partner_id:
             dtype = self.env['res.partner'].browse(
                 partner_id).property_delivery_carrier.id
