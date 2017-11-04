0a43b41a47be60d673bf00932c703abc64b9236f
sergio <sergiocorato@gmail.com>
2017-11-04 16:58:06 +0100

    [FIX] view readonly and function only when dueamount line

--- account_invoice_dueamount/models/account_invoice.py
+++ account_invoice_dueamount/models/account_invoice.py
@@ -35,48 +35,50 @@ class AccountInvoice(models.Model):
     @api.multi
     def finalize_invoice_move_lines(self, move_lines):
         super(AccountInvoice, self).finalize_invoice_move_lines(move_lines)
-        total_dueamount = 0
-        # check total amount lines == invoice.amount_total
-        for dueamount_line in self.dueamount_line:
-            total_dueamount += dueamount_line.amount
-        if total_dueamount != self.amount_total:
-            raise exceptions.ValidationError(
-                _('Total amount of due amount lines must be equal to '
-                  'invoice total amount %d') % self.amount_total)
+        if self.dueamount_line:
+            total_dueamount = 0
+            # check total amount lines == invoice.amount_total
+            for dueamount_line in self.dueamount_line:
+                total_dueamount += dueamount_line.amount
+            if total_dueamount != self.amount_total:
+                raise exceptions.ValidationError(
+                    _('Total amount of due amount lines must be equal to '
+                      'invoice total amount %d') % self.amount_total)
 
-        dueamount_ids = self.dueamount_line.ids
-        maturity_move_lines = [
-            l for l in move_lines if l[2].get('date_maturity', False)]
-        maturity_lines_delta = len(maturity_move_lines) - len(dueamount_ids)
+            dueamount_ids = self.dueamount_line.ids
+            maturity_move_lines = [
+                l for l in move_lines if l[2].get('date_maturity', False)]
+            maturity_lines_delta = \
+                len(maturity_move_lines) - len(dueamount_ids)
 
-        if maturity_lines_delta > 0:
-            # remove extra maturity lines from move_lines
+            if maturity_lines_delta > 0:
+                # remove extra maturity lines from move_lines
+                for line in move_lines:
+                    if line[2].get('date_maturity', False):
+                        if maturity_lines_delta > 0:
+                            move_lines.remove(line)
+                            maturity_lines_delta -= 1
+            # add extra move lines
+            elif maturity_lines_delta < 0:
+                for i in range(0, abs(maturity_lines_delta)):
+                    move_line_copy = copy.deepcopy(
+                        tuple([maturity_move_lines[0]])
+                    )
+                    move_lines += move_line_copy
+
+            dueamount_lines = self.env[
+                'account.invoice.dueamount.line'].browse(dueamount_ids)
+            i = 0
             for line in move_lines:
                 if line[2].get('date_maturity', False):
-                    if maturity_lines_delta > 0:
-                        move_lines.remove(line)
-                        maturity_lines_delta -= 1
-        # add extra move lines
-        elif maturity_lines_delta < 0:
-            for i in range(0, abs(maturity_lines_delta)):
-                move_line_copy = copy.deepcopy(
-                    tuple([maturity_move_lines[0]])
-                )
-                move_lines += move_line_copy
-
-        dueamount_lines = self.env[
-            'account.invoice.dueamount.line'].browse(dueamount_ids)
-        i = 0
-        for line in move_lines:
-            if line[2].get('date_maturity', False):
-                dueamount_line = dueamount_lines[i]
-                is_credit = True if line[2]['credit'] > 0 else False
-                line[2].update({'date_maturity': dueamount_line.date})
-                if is_credit:
-                    line[2].update({'credit': dueamount_line.amount})
-                else:
-                    line[2].update({'debit': dueamount_line.amount})
-                i += 1
+                    dueamount_line = dueamount_lines[i]
+                    is_credit = True if line[2]['credit'] > 0 else False
+                    line[2].update({'date_maturity': dueamount_line.date})
+                    if is_credit:
+                        line[2].update({'credit': dueamount_line.amount})
+                    else:
+                        line[2].update({'debit': dueamount_line.amount})
+                    i += 1
 
         return move_lines
 
