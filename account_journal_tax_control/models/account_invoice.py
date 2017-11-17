4dad920c9eef6dbc16bf55a89aae97ddd9c8367d
sergio <sergiocorato@gmail.com>
2017-11-17 17:32:05 +0100

    [FIX] check only taxes if journal have taxes configured

--- account_journal_tax_control/models/account_invoice.py
+++ account_journal_tax_control/models/account_invoice.py
@@ -16,10 +16,11 @@ class AccountInvoice(models.Model):
         error_template = _("Invoice has line %s with tax not admitted")
         for invoice in self:
             for invoice_line in invoice.invoice_line:
-                if invoice_line.invoice_line_tax_id not in invoice.journal_id.\
-                        account_tax_control_ids:
-                    error_string = error_template % invoice_line.name
-                    errors.append(error_string)
+                if invoice.journal_id.account_tax_control_ids:
+                    if invoice_line.invoice_line_tax_id not in \
+                            invoice.journal_id.account_tax_control_ids:
+                        error_string = error_template % invoice_line.name
+                        errors.append(error_string)
         if errors:
             errors_full_string = ','.join(x for x in errors)
             raise exceptions.Warning(_('Invalid Journal Taxes!'),
