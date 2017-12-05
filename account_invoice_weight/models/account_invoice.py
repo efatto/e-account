3b0ac985c95e522025a399b1ae7c6578e40e2aca
sergio <sergiocorato@gmail.com>
2017-12-05 16:01:25 +0100

    [FIX] invoice weight computation simply

--- account_invoice_weight/models/account_invoice.py
+++ account_invoice_weight/models/account_invoice.py
@@ -56,49 +56,29 @@ class AccountInvoice(models.Model):
     @api.multi
     def _compute_weight(self):
         for invoice in self:
-            # compute from invoice and sppp
-            # sum weight for line without origin
-            for line in invoice.invoice_line:
-                if invoice.stock_picking_package_preparation_ids:
-                    for sppp in invoice.stock_picking_package_preparation_ids:
-                        for picking in sppp.picking_ids:
-                            if line.origin and picking.origin and line.origin \
-                                    not in picking.origin and \
-                                    line.origin not in picking.name:
-                                invoice.weight += line.product_id.\
-                                    weight * line.quantity
-                                invoice.net_weight += line.product_id. \
-                                    weight_net * line.quantity
-                                invoice.volume += line.product_id. \
-                                    volume * line.quantity
-                else: # if not pickings, sum all lines
-                    invoice.weight += line.product_id. \
-                                          weight * line.quantity
-                    invoice.net_weight += line.product_id. \
-                                              weight_net * line.quantity
-                    invoice.volume += line.product_id. \
-                                          volume * line.quantity
-            # then sum weight from sppp (for residual lines)
-            invoice.weight += sum(
-                x.weight for x in
-                invoice.stock_picking_package_preparation_ids)
-            invoice.net_weight += sum(
-                x.net_weight for x in
-                invoice.stock_picking_package_preparation_ids)
-            invoice.volume += sum(
-                x.volume for x in
-                invoice.stock_picking_package_preparation_ids)
-
-            # then compute only from invoice without sppp
-            invoice.net_weight_invoice = sum(
-                l.product_id.weight_net and l.product_id.weight_net
-                * l.quantity for l in invoice.invoice_line)
-            invoice.weight_invoice = sum(
-                l.product_id.weight and l.product_id.weight
-                * l.quantity for l in invoice.invoice_line)
-            invoice.volume_invoice = sum(
-                l.product_id.volume and l.product_id.volume
-                * l.quantity for l in invoice.invoice_line)
+            # sum weight from pickings
+            if invoice.stock_picking_package_preparation_ids:
+                invoice.weight += sum(
+                    x.weight for x in
+                    invoice.stock_picking_package_preparation_ids)
+                invoice.net_weight += sum(
+                    x.net_weight for x in
+                    invoice.stock_picking_package_preparation_ids)
+                invoice.volume += sum(
+                    x.volume for x in
+                    invoice.stock_picking_package_preparation_ids)
+            # compute from invoice if not pickings
+            else:
+                invoice.weight = sum(
+                    l.product_id.weight and l.product_id.weight
+                    * l.quantity for l in invoice.invoice_line)
+                invoice.net_weight = sum(
+                    l.product_id.weight_net and l.product_id.weight_net
+                    * l.quantity for l in invoice.invoice_line)
+                invoice.volume = sum(
+                    l.product_id.volume and l.product_id.volume
+                    * l.quantity for l in invoice.invoice_line)
+            # put custom value if selected from user
             if not invoice.compute_weight:
                 invoice.net_weight = invoice.net_weight_custom
                 invoice.weight = invoice.weight_custom
