fbd29ddc1af1447b52d266391a5ecbac5b0615e5
sergio <sergiocorato@gmail.com>
2018-01-14 18:43:48 +0100

    [IMP] add possibility to hide product code in sale order report

--- sale_order_report_hide_prices/models/sale_order.py
+++ sale_order_report_hide_prices/models/sale_order.py
@@ -8,4 +8,7 @@ from openerp import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    print_hide_prices = fields.Boolean(string='Hide prices in report?')
+    print_hide_prices = fields.Boolean(
+        string='Hide prices in report?')
+    print_hide_product_code = fields.Boolean(
+        string='Hide product code in report?')
