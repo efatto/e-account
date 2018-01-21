68621093d9bd894c9aedd46825c28440f15391eb
sergio <sergiocorato@gmail.com>
2018-01-22 00:46:15 +0100

    [IMP] ddt in invoice tree

--- account_invoice_sppp/models/account.py
+++ account_invoice_sppp/models/account.py
@@ -9,5 +9,5 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     stock_picking_package_preparation_ids = fields.One2many(
-        'stock.picking.package.preparation', 'invoice_id', 'Pickings',
+        'stock.picking.package.preparation', 'invoice_id', 'DDTs',
         groups="stock.group_stock_user", readonly=True)
