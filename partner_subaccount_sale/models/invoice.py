a77aced4c7caae20483246ca1af75d1cdc97c6eb
sergio <sergiocorato@gmail.com>
2018-02-11 22:37:31 +0100

    [FIX] use old api for compatibility

--- partner_subaccount_sale/models/invoice.py
+++ partner_subaccount_sale/models/invoice.py
@@ -8,21 +8,32 @@ from openerp import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    @api.multi
-    def onchange_partner_id(self, type, partner_id):
-        res = super(AccountInvoice, self).onchange_partner_id(type, partner_id)
+    @api.cr_uid_ids_context
+    def onchange_partner_id(
+            self, cr, uid, ids, type, partner_id, date_invoice=False,
+            payment_term=False, partner_bank_id=False, company_id=False,
+            context=None):
+        res = super(AccountInvoice, self).onchange_partner_id(
+            cr, uid, ids, type, partner_id, date_invoice, payment_term,
+            partner_bank_id, company_id, context)
         if not partner_id:
             return res
-        partner = self.env['res.partner'].browse(partner_id)
+        partner = self.pool['res.partner'].browse(cr, uid, partner_id, context)
         if partner.property_account_receivable.type == 'view' and \
                 partner.customer:
-            partner.with_context(create_partner=True).write({'customer': True})
+            ctx = context.copy()
+            ctx['create_partner'] = True
+            self.pool['res.partner'].write(
+                cr, uid, [partner_id], {'customer': True}, context=ctx)
             if type in ['out_invoice', 'out_refund']:
                 res['value'].update(
                     {'account_id': partner.property_account_receivable.id})
         if partner.property_account_payable.type == 'view' and \
                 partner.supplier:
-            partner.with_context(create_partner=True).write({'supplier': True})
+            ctx = context.copy()
+            ctx['create_partner'] = True
+            self.pool['res.partner'].write(
+                cr, uid, [partner_id], {'supplier': True}, context=ctx)
             if type in ['in_invoice', 'in_refund']:
                 res['value'].update(
                     {'account_id': partner.property_account_payable.id})
