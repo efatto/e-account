f839b1dd5ea252fa37274c66d68ae1d6f021b58e
sergio <sergiocorato@gmail.com>
2018-02-23 18:17:23 +0100

    [FIX] layout and discount application only on product and consumable lines of invoice

--- invoice_discount_update/models/invoice.py
+++ invoice_discount_update/models/invoice.py
@@ -8,11 +8,12 @@ from openerp import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    discount = fields.Float(help='If changed will affect all order lines.')
+    discount = fields.Float()
 
     @api.multi
     @api.depends('fiscal_position', 'discount', 'invoice_line')
     def invoice_discount_update(self):
         for invoice in self:
             for line in invoice.invoice_line:
-                line.discount = invoice.discount
+                if line.product_id.type != 'service':
+                    line.discount = invoice.discount
