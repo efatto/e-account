20fa92b33480ab54e620820b9ff0ccc61c9f1119
sergio <sergiocorato@gmail.com>
2018-04-06 16:51:59 +0200

    [FIX] check template email for attach reports

--- email_from_tree/models/mail.py
+++ email_from_tree/models/mail.py
@@ -38,34 +38,35 @@ class MailComposeMessage(models.TransientModel):
         if not template_id:
             raise exceptions.ValidationError(_('No template found!'))
         attachment_ids = []
-        for obj in self.env[self._context['active_model']].browse(
-                self._context['active_ids']):
-            attachment_obj = self.env['ir.attachment']
-            if report:
-                (result, format) = openerp.report.render_report(
-                    self._cr, self._uid, [obj.id],
-                    report.report_name,
-                    {'model': obj._name},
-                    self._context)
-                eval_context = {'time': time,
-                                'object': obj}
-                if not report.attachment or not eval(
-                        report.attachment, eval_context):
-                    # no auto-saving of report as attachment,
-                    # need to do it manually
-                    result = base64.b64encode(result)
-                    file_name = '%s_%s.pdf' % (
-                        obj.partner_id.name,
-                        obj.number if self._context['active_model'] ==
-                        'account.invoice' else obj.name)
-                    attachment_id = attachment_obj.create({
-                            'name': file_name,
-                            'datas': result,
-                            'datas_fname': file_name,
-                            'res_model': obj._name,
-                            'res_id': obj.id,
-                            'type': 'binary'
-                        })
-                    attachment_ids += [attachment_id.id]
-        values[values.keys()[0]].update({'attachment_ids': attachment_ids})
+        if self._context.get('default_template_id', False) == template_id[1]:
+            for obj in self.env[self._context['active_model']].browse(
+                    self._context['active_ids']):
+                attachment_obj = self.env['ir.attachment']
+                if report:
+                    (result, format) = openerp.report.render_report(
+                        self._cr, self._uid, [obj.id],
+                        report.report_name,
+                        {'model': obj._name},
+                        self._context)
+                    eval_context = {'time': time,
+                                    'object': obj}
+                    if not report.attachment or not eval(
+                            report.attachment, eval_context):
+                        # no auto-saving of report as attachment,
+                        # need to do it manually
+                        result = base64.b64encode(result)
+                        file_name = '%s_%s.pdf' % (
+                            obj.partner_id.name,
+                            obj.number if self._context['active_model'] ==
+                            'account.invoice' else obj.name)
+                        attachment_id = attachment_obj.create({
+                                'name': file_name,
+                                'datas': result,
+                                'datas_fname': file_name,
+                                'res_model': obj._name,
+                                'res_id': obj.id,
+                                'type': 'binary'
+                            })
+                        attachment_ids += [attachment_id.id]
+            values[values.keys()[0]].update({'attachment_ids': attachment_ids})
         return values
