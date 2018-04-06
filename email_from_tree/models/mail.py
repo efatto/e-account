0a34b2e151242625643e7eac5bd3ed99dcd99cb6
sergio <sergiocorato@gmail.com>
2018-04-06 17:44:03 +0200

    [FIX] check template mail

--- email_from_tree/models/mail.py
+++ email_from_tree/models/mail.py
@@ -18,15 +18,14 @@ class MailComposeMessage(models.TransientModel):
                        ).generate_email_for_composer_batch(
             template_id, res_ids, context=context, fields=fields
         )
+        report_name = False
+        template = False
         if self._context['active_model'] == 'account.invoice':
             report_name = 'account.report_invoice'
             template = 'email_template_invoice_from_tree'
         elif self._context['active_model'] == 'sale.order':
             report_name = 'sale.report_saleorder'
             template = 'email_template_sale_from_tree'
-        else:
-            report_name = False
-            template = False
         if report_name and template:
             ir_actions_report = self.env['ir.actions.report.xml']
             report = ir_actions_report.search([
@@ -35,38 +34,38 @@ class MailComposeMessage(models.TransientModel):
             template_id = self.env['ir.model.data'].get_object_reference(
                 'email_from_tree', template
             )
-        if not template_id:
-            raise exceptions.ValidationError(_('No template found!'))
-        attachment_ids = []
-        if self._context.get('default_template_id', False) == template_id[1]:
-            for obj in self.env[self._context['active_model']].browse(
-                    self._context['active_ids']):
-                attachment_obj = self.env['ir.attachment']
-                if report:
-                    (result, format) = openerp.report.render_report(
-                        self._cr, self._uid, [obj.id],
-                        report.report_name,
-                        {'model': obj._name},
-                        self._context)
-                    eval_context = {'time': time,
-                                    'object': obj}
-                    if not report.attachment or not eval(
-                            report.attachment, eval_context):
-                        # no auto-saving of report as attachment,
-                        # need to do it manually
-                        result = base64.b64encode(result)
-                        file_name = '%s_%s.pdf' % (
-                            obj.partner_id.name,
-                            obj.number if self._context['active_model'] ==
-                            'account.invoice' else obj.name)
-                        attachment_id = attachment_obj.create({
-                                'name': file_name,
-                                'datas': result,
-                                'datas_fname': file_name,
-                                'res_model': obj._name,
-                                'res_id': obj.id,
-                                'type': 'binary'
-                            })
-                        attachment_ids += [attachment_id.id]
-            values[values.keys()[0]].update({'attachment_ids': attachment_ids})
+            if not template_id:
+                raise exceptions.ValidationError(_('No template found!'))
+            if self._context.get('default_template_id', False) == template_id[1]:
+                attachment_ids = []
+                for obj in self.env[self._context['active_model']].browse(
+                        self._context['active_ids']):
+                    attachment_obj = self.env['ir.attachment']
+                    if report:
+                        (result, format) = openerp.report.render_report(
+                            self._cr, self._uid, [obj.id],
+                            report.report_name,
+                            {'model': obj._name},
+                            self._context)
+                        eval_context = {'time': time,
+                                        'object': obj}
+                        if not report.attachment or not eval(
+                                report.attachment, eval_context):
+                            # no auto-saving of report as attachment,
+                            # need to do it manually
+                            result = base64.b64encode(result)
+                            file_name = '%s_%s.pdf' % (
+                                obj.partner_id.name,
+                                obj.number if self._context['active_model'] ==
+                                'account.invoice' else obj.name)
+                            attachment_id = attachment_obj.create({
+                                    'name': file_name,
+                                    'datas': result,
+                                    'datas_fname': file_name,
+                                    'res_model': obj._name,
+                                    'res_id': obj.id,
+                                    'type': 'binary'
+                                })
+                            attachment_ids += [attachment_id.id]
+                values[values.keys()[0]].update({'attachment_ids': attachment_ids})
         return values
