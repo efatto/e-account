305afb4b9e983d0f7cc987a540bf27df4d8c270d
sergio <sergiocorato@gmail.com>
2018-05-21 12:38:09 +0200

    [IMP] add module to set default sale commission in partner instead of default of agent

---
+++ partner_default_sale_commission/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    default_sale_commission_id = fields.Many2one(
+        comodel_name="sale.commission",
+        string="Default commission",
+        company_dependent=True)
---
+++ partner_default_sale_commission/models/sale_order.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from openerp import api, fields, models, exceptions, _
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def _default_agents(self):
+        agents = []
+        if self.env.context.get('partner_id'):
+            partner = self.env['res.partner'].browse(
+                self.env.context['partner_id'])
+            for agent in partner.agents.filtered(
+                lambda a: a.company_id == self.env.user.company_id
+            ):
+                vals = {
+                    'agent': agent.id,
+                    'commission': partner.default_sale_commission_id.id
+                    or agent.commission.id,
+                }
+                vals['display_name'] = self.env['sale.order.line.agent']\
+                    .new(vals).display_name
+                agents.append(vals)
+        return [(0, 0, x) for x in agents]
+
+    agents = fields.One2many(default=_default_agents)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    default_sale_commission_id = fields.Many2one(
+        comodel_name="sale.commission",
+        string="Default commission",
+        readonly=True)
+
+    @api.multi
+    def onchange_partner_id(self, part):
+        res = super(SaleOrder, self).onchange_partner_id(part)
+        if part:
+            partner_id = self.env['res.partner'].browse(part)
+            res['value'].update({
+                'default_sale_commission_id':
+                    partner_id.default_sale_commission_id.id})
+        return res
