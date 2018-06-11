4883d9f539ac1881e80eb7b9a3c0434893e0fa62
sergio <sergiocorato@gmail.com>
2018-06-11 15:13:17 +0200

    [FIX] remove filter by company for agents

--- partner_default_sale_commission/models/sale_order.py
+++ partner_default_sale_commission/models/sale_order.py
@@ -5,31 +5,6 @@
 from openerp import api, fields, models
 
 
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    @api.model
-    def _default_agents(self):
-        agents = []
-        if self.env.context.get('partner_id'):
-            partner = self.env['res.partner'].browse(
-                self.env.context['partner_id'])
-            for agent in partner.agents.filtered(
-                lambda a: a.company_id == self.env.user.company_id
-            ):
-                vals = {
-                    'agent': agent.id,
-                    'commission': partner.default_sale_commission_id.id
-                    or agent.commission.id,
-                }
-                vals['display_name'] = self.env['sale.order.line.agent']\
-                    .new(vals).display_name
-                agents.append(vals)
-        return [(0, 0, x) for x in agents]
-
-    agents = fields.One2many(default=_default_agents)
-
-
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
