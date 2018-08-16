d21640f1e778d2aa987d2d727affcf96f5911c0e
sergio <sergiocorato@gmail.com>
2018-08-16 10:22:24 +0200

    [FIX] remove unuseful code

--- account_invoice_sppp/models/account.py
+++ account_invoice_sppp/models/account.py
@@ -11,30 +11,3 @@ class AccountInvoice(models.Model):
     stock_picking_package_preparation_ids = fields.One2many(
         'stock.picking.package.preparation', 'invoice_id', 'DDTs',
         groups="stock.group_stock_user", readonly=True)
-
-    @api.multi
-    def unlink(self):
-        invoices = self.filtered(lambda x: x.state == 'draft')
-        pickings = invoices.mapped('stock_picking_package_preparation_ids'
-                                   ).filtered(
-            lambda x: x.state != 'cancel')
-        pickings.write({'invoice_state': '2binvoiced'})
-        return super(AccountInvoice, self).unlink()
-
-    @api.multi
-    def action_cancel(self):
-        res = super(AccountInvoice, self).action_cancel()
-        pickings = self.mapped('stock_picking_package_preparation_ids'
-                               ).filtered(
-            lambda x: x.state != 'cancel')
-        pickings.write({'invoice_state': '2binvoiced'})
-        return res
-
-    @api.multi
-    def action_cancel_draft(self):
-        res = super(AccountInvoice, self).action_cancel_draft()
-        pickings = self.mapped('stock_picking_package_preparation_ids'
-                               ).filtered(
-            lambda x: x.state != 'cancel')
-        pickings.write({'invoice_state': 'invoiced'})
-        return res
