4474556587bb717f728a3a95296c9c67688ec939
sergio <sergiocorato@gmail.com>
2018-08-17 15:17:03 +0200

    [10.0] port due amount module

--- account_invoice_dueamount/models/account_invoice.py
+++ account_invoice_dueamount/models/account_invoice.py
@@ -2,14 +2,14 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import models, api, fields, exceptions, _
+from odoo import models, api, fields, exceptions, _
 import copy
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    dueamount_line = fields.One2many(
+    dueamount_line_ids = fields.One2many(
         comodel_name='account.invoice.dueamount.line',
         inverse_name='invoice_id',
         string='Due amounts',
@@ -18,34 +18,35 @@ class AccountInvoice(models.Model):
     @api.multi
     def dueamount_set(self):
         for invoice in self:
-            if invoice.payment_term and invoice.amount_total:
-                totlines = invoice.payment_term.compute(
+            if invoice.payment_term_id and invoice.amount_total:
+                totlines = invoice.payment_term_id.compute(
                     invoice.amount_total, invoice.date_invoice or False)[0]
                 dueamount_line_obj = self.env['account.invoice.dueamount.line']
-                dueamount_lines = dueamount_line_obj.search([
-                    ('invoice_id', '=', invoice.id)])
-                if dueamount_lines:
-                    dueamount_lines.unlink()
-                # create lines
-                for due_line in totlines:
-                    dueamount_line_obj.create({
-                        'date': due_line[0], 'amount': due_line[1],
-                        'invoice_id': invoice.id})
+                due_line_ids = []
+                for line in totlines:
+                    due_line_id = dueamount_line_obj.create({
+                        'date': line[0],
+                        'amount': line[1],
+                        'invoice_id': invoice.id,
+                    })
+                    due_line_ids.append(due_line_id.id)
+                invoice.write({
+                    'dueamount_line_ids': [(6, 0, due_line_ids)]})
 
     @api.multi
     def finalize_invoice_move_lines(self, move_lines):
         super(AccountInvoice, self).finalize_invoice_move_lines(move_lines)
-        if self.dueamount_line:
+        if self.dueamount_line_ids:
             total_dueamount = 0
             # check total amount lines == invoice.amount_total
-            for dueamount_line in self.dueamount_line:
+            for dueamount_line in self.dueamount_line_ids:
                 total_dueamount += dueamount_line.amount
             if total_dueamount != self.amount_total:
                 raise exceptions.ValidationError(
                     _('Total amount of due amount lines must be equal to '
                       'invoice total amount %d') % self.amount_total)
 
-            dueamount_ids = self.dueamount_line.ids
+            dueamount_ids = self.dueamount_line_ids.ids
             maturity_move_lines = [
                 l for l in move_lines if l[2].get('date_maturity', False)]
             maturity_lines_delta = \
