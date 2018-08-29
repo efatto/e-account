1a050c1ecf9aa3df3e9f996a78c75fc600a1e419
sergio <sergiocorato@gmail.com>
2018-08-29 15:48:38 +0200

    [10.0] port base vat sanitize

---
+++ base_vat_sanitized_unique/models/partner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, _, api, exceptions
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.constrains('vat')
+    @api.multi
+    def vat_onchange(self):
+        for p in self:
+            if p.vat:
+                duplicated_partner_ids = self.env['res.partner'].search([
+                    ('sanitized_vat', '=', p.vat.upper().replace(' ', '')),
+                    ('id', '!=', p.id),
+                    ('parent_id', '=', False)])
+                if duplicated_partner_ids:
+                    raise exceptions.ValidationError(
+                        _('This VAT is already registered with %s partners') %
+                        duplicated_partner_ids.mapped('name'))
