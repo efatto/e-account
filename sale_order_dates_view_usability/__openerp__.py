209de96b696bc439f11d38448278f9aba7a6a0c0
sergio <sergiocorato@gmail.com>
2018-08-29 16:31:19 +0200

    [10.0] port sale order dates menu usability

--- sale_order_dates_view_usability/__openerp__.py
+++ sale_order_dates_view_usability/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
-#    Copyright (C) 2017 Sergio Corato
+#    Copyright (C) 2017-2018 Sergio Corato
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -19,12 +19,12 @@
 #
 {
     'name': 'Sale order dates view usability',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'This module move sale order ref in tree view after '
                    'partner.',
-    'website': 'http://www.efatto.it',
+    'website': 'https://efatto.it',
     'license': 'AGPL-3',
     'depends': [
         'sale_order_dates',
@@ -32,5 +32,5 @@
     'data': [
         'views/sale_order_view.xml',
     ],
-    'installable': False
+    'installable': True,
 }
