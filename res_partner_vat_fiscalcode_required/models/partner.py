177812e4a7c8ca47018e1ad7aa525e2485a42a70
sergiocorato <sergiocorato@gmail.com>
2019-02-13 10:33:47 +0100

    [FIX] require vat or fiscalcode only for italian partners

--- res_partner_vat_fiscalcode_required/models/partner.py
+++ res_partner_vat_fiscalcode_required/models/partner.py
@@ -2,4 +2,11 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from . import models
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    country_code = fields.Char(
+        related='country_id.code', string='Country Code')
