737b5c3bccf60f23cd6bfa92827e22717612bf11
sergiocorato <sergiocorato@gmail.com>
2019-07-24 18:16:04 +0200

    [IMP] add boolean to print net prices

--- sale_order_report_hide_prices/models/sale_order.py
+++ sale_order_report_hide_prices/models/sale_order.py
@@ -9,6 +9,8 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     print_hide_prices = fields.Boolean(
-        string='Hide prices in report?')
+        string='Hide prices in report')
     print_hide_product_code = fields.Boolean(
-        string='Hide product code in report?')
+        string='Hide product code in report')
+    print_net_prices = fields.Boolean(
+        string='Print net prices')
