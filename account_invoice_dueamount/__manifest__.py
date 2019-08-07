51eef9f3d4c83a9c5e0d48005e52e4131b916aa0
sergiocorato <sergiocorato@gmail.com>
2019-08-07 23:19:26 +0200

    [FIX] compare float

--- account_invoice_dueamount/__manifest__.py
+++ account_invoice_dueamount/__manifest__.py
@@ -1,31 +1,15 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2017-2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright (C) 2017-2019 Sergio Corato
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl
 {
     'name': 'Account invoice due amount',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'This module add optional custom due amount field, '
                    'to customize amount and dates of payments.',
     'website': 'https://efatto.it',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'depends': [
         'account',
     ],
