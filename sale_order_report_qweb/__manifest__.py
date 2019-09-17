1fd21afa0ee2845d050594e1653cf4f9893f95d6
sergiocorato <sergiocorato@gmail.com>
2019-09-17 08:10:02 +0200

    [IMP] hidden sections

--- sale_order_report_qweb/__manifest__.py
+++ sale_order_report_qweb/__manifest__.py
@@ -18,7 +18,7 @@
 #
 {
     'name': 'Qweb sale order report',
-    'version': '10.0.1.0.2',
+    'version': '10.0.1.0.3',
     'category': 'other',
     'author': 'Sergio Corato - Efatto.it',
     'website': 'https://efatto.it',
