f1a46ab4737f22327283333d15f0cf91daa36c3e
sergiocorato <sergiocorato@gmail.com>
2019-09-22 00:22:45 +0200

    [IMP] translation

--- l10n_it_fatturapa_invoice_advance_auto/__openerp__.py
+++ l10n_it_fatturapa_invoice_advance_auto/__openerp__.py
@@ -2,11 +2,12 @@
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
-    'name': 'Sale advance account invoice',
+    'name': 'E-invoice advance auto',
     'version': '8.0.1.0.0',
     'category': 'Sale',
     'author': 'Sergio Corato',
-    'description': 'Add boolean to mark advance invoices',
+    'description': 'Add boolean to mark advance invoices when they are created'
+                   ' from sale order',
     'website': 'https://efatto.it',
     'license': 'LGPL-3',
     'depends': [
