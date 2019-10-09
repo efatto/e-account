ab06f617bf04f3f8a97ea3128aee678adcbc9611
sergiocorato <sergiocorato@gmail.com>
2019-10-09 15:42:00 +0200

    [FIX] use constraint on vat

--- base_vat_sanitize_unique/models/partner.py
+++ base_vat_sanitize_unique/models/partner.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+
 from openerp import models, _, api, exceptions
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    @api.constrains('vat')
     @api.multi
-    def vat_change(self, vat):
-        if vat:
-            duplicated_partner_ids = self.env['res.partner'].search([
-                ('sanitized_vat', '=', vat.upper().replace(' ', '')),
-                ('id', '!=', self.id),
-                ('parent_id', '=', False),
-                ('fiscalcode', '=', self.fiscalcode),
-            ])
-            if duplicated_partner_ids:
-                raise exceptions.ValidationError(
-                    _('This VAT is already registered with %s partners') %
-                    duplicated_partner_ids.mapped('name')
-                )
-        return super(ResPartner, self).vat_change(vat)
+    def vat_onchange(self):
+        for p in self:
+            if p.vat and not p.parent_id:
+                duplicated_partner_ids = self.env['res.partner'].search([
+                    ('sanitized_vat', '=', p.vat.upper().replace(' ', '')),
+                    ('id', '!=', p.id),
+                    ('parent_id', '=', False),
+                    ('fiscalcode', '=', self.fiscalcode)])
+                if duplicated_partner_ids:
+                    raise exceptions.ValidationError(
+                        _('This VAT is already registered with %s partners') %
+                        duplicated_partner_ids.mapped('name'))
