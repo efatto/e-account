b237c0cacce369f479768bbca2324bee22611740
sergiocorato <sergiocorato@gmail.com>
2019-11-05 11:32:31 +0100

    [FIX] leave field datetime for compatibility - hide committment date

--- sale_order_dates_view_usability/models/sale_order.py
+++ sale_order_dates_view_usability/models/sale_order.py
@@ -12,8 +12,8 @@ class SaleOrder(models.Model):
         for order in self:
             if self.env['ir.config_parameter'].get_param('sale_default_lead_days') and \
                     not order.requested_date:
-                order.requested_date = fields.Date.to_string(
-                    fields.Date.from_string(fields.Date.today())
+                order.requested_date = fields.Datetime.to_string(
+                    fields.Datetime.from_string(fields.Datetime.now())
                     + relativedelta(days=int(
                         self.env['ir.config_parameter'].get_param(
                             'sale_default_lead_days'))
@@ -22,15 +22,9 @@ class SaleOrder(models.Model):
         res = super(SaleOrder, self).action_button_confirm()
         return res
 
-    requested_date = fields.Date(
+    requested_date = fields.Datetime(
         readonly=False,
         states={'cancel': [('readonly', True)], 'done': [('readonly', True)]},
         copy=False,
         help="If not set, it will be filled at the confirmation of sale order to "
              "(today + sale_default_lead_days parameter).")
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    requested_date = fields.Date()
