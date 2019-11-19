c25a8ab5933bf05bf049a5e9c2afe282da4e4d33
sergiocorato <sergiocorato@gmail.com>
2019-11-19 09:11:26 +0100

    [REV] to original version

--- account_invoice_mail/__openerp__.py
+++ account_invoice_mail/__openerp__.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2019 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+#    Copyright (C) 2017 Sergio Corato
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
 {
     'name': 'Account invoice mail',
-    'version': '8.0.1.1.0',
+    'version': '8.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
-    'description': 'Button to send mail for invoice always visible.'
-                   'Add field Invoice Email for simpler management.',
+    'description': 'Button to send mail for invoice always visible.',
     'website': 'http://www.efatto.it',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'depends': [
         'account',
     ],
     'data': [
         'views/account_invoice_view.xml',
-        'views/res_partner_view.xml',
     ],
-    'external_dependencies': {
-        'python': [
-            'email_validator',
-        ],
-    },
     'installable': True
 }
