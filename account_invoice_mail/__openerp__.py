c88e66466dbf684dcdcfe12ed7b3f10028e284b0
sergiocorato <sergiocorato@gmail.com>
2019-11-19 09:45:58 +0100

    [FIX] name

--- account_invoice_mail/__openerp__.py
+++ account_invoice_mail/__openerp__.py
@@ -2,13 +2,13 @@
 # Copyright 2017-2019 Sergio Corato <https://github.com/sergiocorato>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
-    'name': 'Account invoice mail address',
+    'name': 'Account invoice mail validate',
     'version': '8.0.1.1.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': 'Add field Invoice Email - checked with email_validator - '
                    'for simpler management.',
-    'website': 'http://www.efatto.it',
+    'website': 'https://efatto.it',
     'license': 'LGPL-3',
     'depends': [
         'account',
