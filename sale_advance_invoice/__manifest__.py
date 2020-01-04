0e966b7bf68184610113eedbe459c7b4d674bad0
sergiocorato <sergiocorato@gmail.com>
2020-01-04 16:56:03 +0100

    index on 12.0: 55ed834 WIP on 12.0: 0ca92df [PORT] invoice update discount

---
+++ sale_advance_invoice/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2017-2019 Sergio Corato
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Sale advance invoice description from sale order lines',
+    'version': '10.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Add ability to select order lines to print details in the'
+                   'invoice',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale.xml',
+    ],
+    'installable': True,
+}
