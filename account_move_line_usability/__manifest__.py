65f1fd3a14b895d5427f8f8cbeccdbe834258910
sergiocorato <sergiocorato@gmail.com>
2020-02-23 15:57:56 +0100

    index on 12.0: 7588d34 [PORT] vat sanitized constraint

---
+++ account_move_line_usability/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2017-2018 Sergio Corato
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Account move line usability',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'author': 'Sergio Corato',
+    'description': '''
+Solve some usability issue in account move line:
+------------------------------------------------
+* set credit accordingly to debit and viceversa,
+* add filter from_date and to_date,
+* set minimum width for account fields,
+* set next line with balance value.
+''',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/css.xml',
+        'views/account.xml',
+    ],
+    'installable': True,
+}
---
+++ account_move_line_usability/models/account.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from odoo import models, fields, api, _
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.onchange('credit')
+    def _credit_onchange(self):
+        if self.credit and self.debit:
+            self.debit = 0
+
+    @api.onchange('debit')
+    def _debit_onchange(self):
+        if self.debit and self.credit:
+            self.credit = 0
+
+    name = fields.Text('Name', required=True)
+    date_from = fields.Date(
+        compute=lambda *a, **k: {},
+        string="Date from")
+    date_to = fields.Date(
+        compute=lambda *a, **k: {},
+        string="Date to")
+
+    @api.model
+    def default_get(self, fields):
+        data = super(AccountMoveLine, self).default_get(fields)
+        if data and self._context.get('line_ids', False):
+            debit = credit = 0
+            for x in self._context['line_ids']:
+                debit += x[2]['debit']
+            for x in self._context['line_ids']:
+                credit += x[2]['credit']
+            balance = credit - debit
+            if balance > 0:
+                data['debit'] = balance
+            elif balance < 0:
+                data['credit'] = abs(balance)
+        return data
