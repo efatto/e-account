d40e1129b56aef205dfb3cb76890e6568718d2f1
sergiocorato <sergiocorato@gmail.com>
2020-02-23 16:14:51 +0100

    [PORT] account move line usability

--- account_move_line_usability/__manifest__.py
+++ account_move_line_usability/__manifest__.py
@@ -1,25 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2017-2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
 {
     'name': 'Account move line usability',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'author': 'Sergio Corato',
     'description': '''
@@ -28,7 +12,8 @@ Solve some usability issue in account move line:
 * set credit accordingly to debit and viceversa,
 * add filter from_date and to_date,
 * set minimum width for account fields,
-* set next line with balance value.
+* set next line with balance value,
+* set account accordingly to partner.
 ''',
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
