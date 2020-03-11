41d52a15a3d6241c4f4e1e927584765ead38e265
sergiocorato <sergiocorato@gmail.com>
2020-03-11 15:50:06 +0100

    [FIX] modify existing move

--- account_move_line_usability/__manifest__.py
+++ account_move_line_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account move line usability',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'category': 'Accounting & Finance',
     'author': 'Sergio Corato',
     'description': '''
