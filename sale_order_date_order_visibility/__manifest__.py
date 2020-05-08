c2f42879c43f5dbfe0d204e1cf9f5dd064240416
sergiocorato <sergiocorato@gmail.com>
2020-05-08 15:23:47 +0200

    [ADD] module to show date order always

---
+++ sale_order_date_order_visibility/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Sale order date visibility',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Mode date_order for quotation in header for better visibility',
+    'website': 'https://efatto.it',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+}
