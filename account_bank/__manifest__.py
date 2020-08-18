4cbc7b395ecdfa5dfb88471eea7e63887736f9af
sergiocorato <sergiocorato@gmail.com>
2020-08-18 11:26:36 +0200

    [FIX] migration

--- account_bank/__manifest__.py
+++ account_bank/__manifest__.py
@@ -1,23 +1,18 @@
-# Copyright 2014 Didotech SRL (<http://www.didotech.com>).
-# Copyright 2015-2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "Bank for riba and payment in partner",
+    "name": "Company Bank in partner",
     "version": "12.0.1.0.0",
-    "category": "Localisation",
-    "author": "Didotech SRL, SimplERP SRL, Sergio Corato",
+    "category": "other",
+    "author": "Sergio Corato",
     "website": "https://efatto.it",
-    "description": "Add bank for riba and payment in partner and invoice",
-    "license": "AGPL-3",
+    "description": "Add company dependent bank in partner",
+    "license": "LGPL-3",
     "depends": [
-        "account",
-        "sale",
-        "l10n_it_ricevute_bancarie",
+        "base",
     ],
     "data": [
-        "views/account_invoice_view.xml",
         "views/partner_view.xml",
-        "views/sale_order_report.xml",
     ],
     "installable": True,
 }
---
+++ account_bank/migrations/12.0.1.0.0/post-migration.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.convert_to_company_dependent(
+        env=env,
+        model_name="res.partner",
+        origin_field_name="company_bank_id",
+        destination_field_name="property_company_bank_id",
+    )
