5ed50c9d57d475b525a4ed41801e42b4783261f1
sergiocorato <sergiocorato@gmail.com>
2020-08-18 22:48:43 +0200

    [ADD] payment in invoice tree

---
+++ account_invoice_tree_payment/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Payment visibily in account invoice tree",
+    "version": "12.0.1.0.0",
+    "category": "Localisation",
+    "author": "Sergio Corato",
+    "website": "https://efatto.it",
+    "description": "Add payment in invoice tree",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}
