ce52f2e92d90e253fbf1a17c08a9c9542a8d437b
sergiocorato <sergiocorato@gmail.com>
2020-10-22 23:22:45 +0200

    [IMP] other usability changes

--- sale_order_date_order_visibility/__manifest__.py
+++ sale_order_date_order_visibility/__manifest__.py
@@ -2,14 +2,16 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Sale order date visibility',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'category': 'other',
     'author': 'Sergio Corato',
-    'description': 'Move date_order for quotation in header for better visibility',
+    'description': 'Move date_order for quotation in header for better visibility '
+                   'with opportunity, client order ref, project and analytic.',
     'website': 'https://efatto.it',
     'license': 'LGPL-3',
     'depends': [
-        'sale',
+        'sale_crm',
+        'sale_timesheet_existing_project',
     ],
     'data': [
         'views/sale_order_view.xml',
