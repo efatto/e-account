13d19e8a2ef7e912ea5a608c773a1af828287133
sergiocorato <sergiocorato@gmail.com>
2020-11-13 08:20:41 +0100

    index on 12.0: 1e2b5db [IMP] split sale order ref in tree

---
+++ sale_advance_invoice_sal/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Copyright (C) 2017-2019 Sergio Corato
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': 'Sale advance invoice extension with SAL',
+    'version': '10.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Add ability to get description from SAL of contract during'
+                   ' advance invoice creation. Link to SAL on analytic account'
+                   '.',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_advance_invoice',
+        'account_analytic_sal',
+    ],
+    'data': [
+        'views/sale.xml',
+    ],
+    'installable': True
+}
