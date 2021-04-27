06a46591dbddb3ed4e0b531fb64e8bb6ea85ee3b
sergiocorato <sergiocorato@gmail.com>
2021-04-27 19:20:36 +0200

    [IMP] split module sale_order_date_order_visibility

---
+++ sale_project_visibility/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale project visibility',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Move opportunity, project and analytic for quotation in header '
+                   'for better visibility.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'sale_crm',
+        'sale_timesheet_existing_project',
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+}
