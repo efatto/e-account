97b737fe28d3365c90b0e7b2171ca90e9fd71e60
sergiocorato <sergiocorato@gmail.com>
2021-06-07 18:08:28 +0200

    [IMP] desc

--- sale_commission_view_hide_agents/__openerp__.py
+++ sale_commission_view_hide_agents/__openerp__.py
@@ -18,11 +18,11 @@
 #
 #
 {
-    'name': 'Order sale commission agents line view',
+    'name': 'Order sale line tree commission agents hide',
     'version': '8.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
-    'description': 'Restrict view of agents in sale order line to admin only, '
+    'description': 'Hide view of agents in sale order line, '
                    'for view performance.',
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
