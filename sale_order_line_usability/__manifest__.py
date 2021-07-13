9570eb3ddb288b5169a082540bcd65d0be29bf31
sergiocorato <sergiocorato@gmail.com>
2021-07-13 10:17:48 +0200

    [IMP] add module sale line usability

---
+++ sale_order_line_usability/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale order line usability',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Add order line product standard price link.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
