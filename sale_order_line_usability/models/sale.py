a178e2941127a8866575119b9e5f5f1722f448a8
sergiocorato <sergiocorato@gmail.com>
2021-08-24 14:27:41 +0200

    [IMP] add computed field for total cost to invoice

--- sale_order_line_usability/models/sale.py
+++ sale_order_line_usability/models/sale.py
@@ -10,11 +10,15 @@ class SaleOrderLine(models.Model):
     product_standard_price = fields.Float(
         string="Product Cost", related='product_id.standard_price')
     move_price_unit = fields.Float(
-        string="Move Price Unit", compute='_get_move_price_unit', store=True,
-    )
+        string="Move Price Unit", compute='_get_move_price_unit', store=True)
+    move_price_to_invoice_total = fields.Float(
+        string="Total Cost To Invoice", compute='_get_move_price_unit', store=True)
 
     @api.multi
     @api.depends('move_ids.price_unit')
     def _get_move_price_unit(self):
         for line in self:
             line.move_price_unit = max(line.move_ids.mapped('price_unit') or [0])
+            line.move_price_to_invoice_total = line.qty_to_invoice * (
+                line.move_price_unit if line.move_price_unit != 0.0 else
+                - line.product_standard_price)
