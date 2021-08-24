a39421f3ddae14af74f575b3cc84f528cabfcb9f
sergiocorato <sergiocorato@gmail.com>
2021-08-24 12:04:39 +0200

    [IMP] add total price move

--- account_invoice_line_usability/models/account_invoice_line.py
+++ account_invoice_line_usability/models/account_invoice_line.py
@@ -8,17 +8,18 @@ class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     account_code = fields.Char(
-        related='account_id.code'
-    )
+        related='account_id.code')
     product_standard_price = fields.Float(
-        related='product_id.standard_price'
-    )
+        related='product_id.standard_price')
     move_price_unit = fields.Float(
-        string="Move Price Unit", compute='_get_move_price_unit', store=True,
-    )
+        string="Move Price Unit", compute='_get_move_price_unit', store=True)
+    move_price_total = fields.Float(
+        string="Price Total", compute='_get_move_price_unit', store=True)
 
     @api.multi
     @api.depends('move_line_ids.price_unit')
     def _get_move_price_unit(self):
         for line in self:
             line.move_price_unit = max(line.move_line_ids.mapped('price_unit') or [0])
+            line.move_price_total = line.quantity * -1 * max(
+                [abs(line.move_price_unit), abs(line.product_standard_price)])
