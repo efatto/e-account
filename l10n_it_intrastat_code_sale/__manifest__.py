4f70b4fd97af41bff507d5b01c5daa2e7f5fabe1
sergiocorato <sergiocorato@gmail.com>
2021-10-18 12:41:59 +0200

    [ADD] module for intrastat sale

---
+++ l10n_it_intrastat_code_sale/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Intrastat code for out invoice/refund',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Add ability to use different intrastat code from sale to purchase.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'l10n_it_intrastat',
+    ],
+    'installable': True,
+}
