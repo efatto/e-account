416690cea38c031e83db5f7e2660aa9fb6e5e3f4
sergiocorato <sergiocorato@gmail.com>
2022-01-11 10:35:54 +0100

    [PORT] account sequence accountant

--- account_sequence_accountant/__manifest__.py
+++ account_sequence_accountant/__manifest__.py
@@ -1,37 +1,18 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2017-2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2017-2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sequence access for accountant',
-    'version': '10.0.1.0.0',
+    'name': 'Sequence access to accountant',
+    'version': '12.0.1.0.0',
     'category': 'other',
-    'description': 'Add ability to view and modify sequence from accountant '
-                   'users, without need of administrator in debug mode.',
+    'description': 'Add ability to modify sequence from accountant users',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
     'license': 'AGPL-3',
-    "depends": [
+    'depends': [
         'account',
     ],
-    "data": [
+    'data': [
         'security/ir.model.access.csv',
-        'views/account.xml',
     ],
     'installable': True,
 }
