08ccc9f61304e62dd6da9cc7f6c7c03cb0e317a6
sergiocorato <sergiocorato@gmail.com>
2022-01-11 15:54:27 +0100

    [IMP] vat statement order and important in css

---
+++ account_vat_statement_report/models/vat_statement.py
@@ -0,0 +1,8 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class AccountVatPeriodEndStatement(models.Model):
+    _inherit = "account.vat.period.end.statement"
+    order = 'date desc'
