c70772e382ffa7b4953006a7363cad5fc58e8b8e
sergiocorato <sergiocorato@gmail.com>
2022-01-20 13:07:16 +0100

    [ADD] module to show payment in m2o mode in partners

---
+++ res_partner_payment_term_m2o/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Partner button view account moves',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'author': 'Sergio Corato',
+    'description': 'With this module partner will have normal selection of '
+                   'payment term.',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_payment_partner',
+    ],
+    'data': [
+        'views/partner_account_view.xml',
+    ],
+    'installable': True,
+}
