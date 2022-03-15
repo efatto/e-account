ae345853839b46afb3091255fdc6bc81b2793af8
sergiocorato <sergiocorato@gmail.com>
2022-03-15 11:39:48 +0100

    [FIX] license

--- account_invoice_dueamount/models/account_invoice.py
+++ account_invoice_dueamount/models/account_invoice.py
@@ -1,3 +1,5 @@
+# Copyright 2017-2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models, api, fields, exceptions, _
 from odoo.tools import float_compare
 import copy
