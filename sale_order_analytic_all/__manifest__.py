781d710ca178f622bd86485e930eee83a7bc7656
sergiocorato <sergiocorato@gmail.com>
2022-04-05 19:25:31 +0200

    [ADD] simple module to create analytic account for every SO

---
+++ sale_order_analytic_all/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale order analytic all',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Simple module that create analytic account for every order at '
+                   'creation.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
