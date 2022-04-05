7bc52d1e7ee274b147c1985c89b464abcd414e71
sergiocorato <sergiocorato@gmail.com>
2022-04-05 19:25:31 +0200

    [ADD] simple module to create analytic account for every SO

---
+++ sale_order_analytic_all/models/sale.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def create(self, vals):
+        result = super(SaleOrder, self).create(vals)
+        if not result.analytic_account_id:
+            result._create_analytic_account()
+        return result
