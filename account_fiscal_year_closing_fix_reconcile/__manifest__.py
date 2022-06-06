06dd1fd6224427ff82ea381478930513bf368b52
sergiocorato <sergiocorato@gmail.com>
2022-06-06 18:24:01 +0200

    [IMP] create module from stale PR #141

---
+++ account_fiscal_year_closing_fix_reconcile/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Account fiscal year closing fix reconcile',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'summary': 'Account fiscal year closing fix reconcile.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'account_fiscal_year_closing',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
