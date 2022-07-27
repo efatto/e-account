06078247655d76dd9ce672e0c7d7c95967948ebc
sergiocorato <sergiocorato@gmail.com>
2022-07-27 15:15:34 +0200

    [IMP] translate

--- l10n_it_ricevute_bancarie_unsettlement/__manifest__.py
+++ l10n_it_ricevute_bancarie_unsettlement/__manifest__.py
@@ -1,18 +1,21 @@
-# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'ITA - Ricevute bancarie smarca pagamento',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting',
-    'summary': 'Annulla l\'effetto del bottone marca come pagata',
-    'author': 'Sergio Corato',
-    'website': 'https://efatto.it',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_it_ricevute_bancarie',
+    "name": "ITA - Ricevute bancarie smarca pagamento",
+    "summary": "Annulla l\"effetto del bottone marca come pagata",
+    "version": "12.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Accounting",
+    "website": "https://efatto.it",
+    "author": "Sergio Corato",
+    "maintainers": ["sergiocorato"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_it_ricevute_bancarie",
     ],
-    'data': [
-        'views/riba_view.xml',
+    "data": [
+        "views/riba_view.xml",
     ],
-    'installable': True
 }
