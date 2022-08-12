79735b815d10c35f872f62361c88d5497233e3a5
sergiocorato <sergiocorato@gmail.com>
2022-08-12 15:46:25 +0200

    [IMP] remove auto-subscribe in various functions

--- mail_no_autofollow/models/mail_thread.py
+++ mail_no_autofollow/models/mail_thread.py
@@ -4,11 +4,26 @@ from odoo import api, models
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        # Set default to no subscribe
+        return super(MailThread, self.with_context(mail_create_nosubscribe=True)
+                     ).create(vals_list=vals_list)
+
     @api.multi
     @api.returns('self', lambda value: value.id)
     def message_post(self, *args, **kwargs):
         """Force to no autofollow as in many models (sale.order, purchase.order, ...) it
         is set to True by default.
         """
-        return super(MailThread, self.with_context(mail_post_autofollow=False)
-                     ).message_post(*args, **kwargs)
+        mail_post_autofollow = False
+        return super(MailThread, self.with_context(
+            mail_post_autofollow=mail_post_autofollow)
+        ).message_post(*args, **kwargs)
+
+    def _message_auto_subscribe_followers(self, updated_values, default_subtype_ids):
+        # Remove auto-subscribe by default
+        super()._message_auto_subscribe_followers(
+            updated_values=updated_values, default_subtype_ids=default_subtype_ids
+        )
+        return []
