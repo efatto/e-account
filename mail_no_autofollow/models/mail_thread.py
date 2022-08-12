f07b15540db4648281027cbda8af415cfc6bcd7d
sergiocorato <sergiocorato@gmail.com>
2022-08-12 09:47:54 +0200

    [ADD] module to disable autofollow

---
+++ mail_no_autofollow/models/mail_thread.py
@@ -0,0 +1,14 @@
+from odoo import api, models
+
+
+class MailThread(models.AbstractModel):
+    _inherit = 'mail.thread'
+
+    @api.multi
+    @api.returns('self', lambda value: value.id)
+    def message_post(self, *args, **kwargs):
+        """Forse to no autofollow as in many models (sale.order, purchase.order, ...) it
+        is set to True by default.
+        """
+        return super(MailThread, self.with_context(mail_post_autofollow=False)
+                     ).message_post(*args, **kwargs)
