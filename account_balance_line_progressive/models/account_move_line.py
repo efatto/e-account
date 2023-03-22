33f9593e3fc6ee9eaafe8f10f76c0edd7fa37f5d
sergiocorato <sergiocorato@gmail.com>
2023-03-22 15:33:51 +0100

    [IMP] add currency balance progressive

--- account_balance_line_progressive/models/account_move_line.py
+++ account_balance_line_progressive/models/account_move_line.py
@@ -13,7 +13,9 @@ class AccountMoveLine(models.Model):
         tables, where_clause, where_params = self.with_context(
             initial_bal=True)._query_get()
         where_params = [tuple(self.ids)] + where_params
-        query = """SELECT l1.id, COALESCE(SUM(l2.debit-l2.credit), 0)
+        query = """SELECT l1.id,
+            COALESCE(SUM(l2.debit-l2.credit), 0),
+            COALESCE(SUM(l2.amount_currency), 0)
             FROM account_move_line l1
             LEFT JOIN account_account a
             ON (a.id = l1.account_id)
@@ -36,11 +38,19 @@ class AccountMoveLine(models.Model):
             query += where_clause
         query += " GROUP BY l1.id"
         self._cr.execute(query, where_params)
-        for line_id, val in self._cr.fetchall():
-            self.browse(line_id).balance_progressive = val
+        for line_id, balance, balance_currency in self._cr.fetchall():
+            line = self.browse(line_id)
+            line.balance_progressive = balance
+            line.balance_progressive_currency = balance_currency
 
     balance_progressive = fields.Monetary(
         compute='_compute_balance_progressive',
         currency_field='company_currency_id',
         string='Progressive Balance',
-        help="Field holding the progressive debit - credit of the account")
+        help="Field holding the progressive balance of the account for partner")
+    balance_progressive_currency = fields.Monetary(
+        compute='_compute_balance_progressive',
+        currency_field='currency_id',
+        string='Progressive Balance Currency',
+        help="Field holding the progressive currency balance of the account for "
+             "partner")
