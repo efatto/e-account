645e5e0b448d7d60f63c9abdaa0aee42ab6c9cb7
sergiocorato <sergiocorato@gmail.com>
2023-03-24 15:26:01 +0100

    [FIX] api unused

--- account_balance_line_progressive/models/account_move_line.py
+++ account_balance_line_progressive/models/account_move_line.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class AccountMoveLine(models.Model):
