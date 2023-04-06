18451d0ef9d8317dd7bb5f024492d252647c9edb
sergiocorato <sergiocorato@gmail.com>
2023-04-06 14:31:48 +0200

    [ADD] module to view client order ref in kanban sale view

---
+++ sale_view_client_order_ref/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale view reference in kanban',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'author': 'Sergio Corato',
+    'website': 'https://github.com/sergiocorato/e-account',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale.xml',
+    ],
+    'installable': True,
+}
