8bab7bbfbcad8bf3702569bab3e9dc388730b34d
sergiocorato <sergiocorato@gmail.com>
2023-04-17 16:24:24 +0200

    [FIX] readme

--- mis_builder_query_drilldown_view/__manifest__.py
+++ mis_builder_query_drilldown_view/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Mis builder analytic view",
+    "name": "Mis builder drilldown view",
     "version": "12.0.1.0.0",
     "category": "Reporting",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-account",
     "license": "AGPL-3",
-    "summary": "Add custom analytic view to mis report",
+    "summary": "Add drilldown views to mis report",
     "depends": [
         "account_invoice_line_usability",
         "mis_builder_analytic_query",
