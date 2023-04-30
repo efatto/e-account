106bb945ab980de34deb0c08612c2d1990517370
sergiocorato <sergiocorato@gmail.com>
2023-04-30 22:01:39 +0200

    [WIP] refactoring of mis builder cash flow

---
+++ mis_builder_cash_flow_inheritable/models/cash_flow_line.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class CashFlowForecastLine(models.Model):
+    _inherit = 'mis.cash_flow.forecast_line'
+    _rec_name = 'date'
+
+    res_id = fields.Integer()
+    res_model_id = fields.Many2one(
+        comodel_name='ir.model',
+        index=True)
+    res_model = fields.Char(
+        related='res_model_id.model',
+        store=True)
+    currency_id = fields.Many2one(
+        comodel_name='res.currency'
+    )
+    currency_rate = fields.Float(
+        related='currency_id.rate',
+    )
