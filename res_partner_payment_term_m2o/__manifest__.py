0270a30a6662447230819391715cf96367db621a
sergiocorato <sergiocorato@gmail.com>
2023-06-06 16:22:58 +0200

    [MIG] module res_partner_payment_term_m2o to 14.0

--- res_partner_payment_term_m2o/__manifest__.py
+++ res_partner_payment_term_m2o/__manifest__.py
@@ -1,19 +1,17 @@
-# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2022-2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Partner payment term m2o',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'author': 'Sergio Corato',
-    'description': 'With this module partner will have normal selection of '
-                   'payment term.',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_payment_partner',
+    "name": "Partner payment term m2o",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_partner",
     ],
-    'data': [
-        'views/partner_account_view.xml',
+    "data": [
+        "views/partner_account_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
