b1d97a0751e0ce5994345d0ebb781d98ba714470
sergiocorato <sergiocorato@gmail.com>
2023-06-06 17:28:40 +0200

    [IMP] account_invoice_dueamount: black, isort, prettier

--- account_invoice_dueamount/__manifest__.py
+++ account_invoice_dueamount/__manifest__.py
@@ -1,20 +1,20 @@
-# Copyright 2017-2022 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2017-2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Account invoice due amount',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'This module add optional custom due amount field, '
-                   'to customize amount and dates of payments.',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Account invoice due amount",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "description": "This module add optional custom due amount field, "
+    "to customize amount and dates of payments.",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/account_invoice_view.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_invoice_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
