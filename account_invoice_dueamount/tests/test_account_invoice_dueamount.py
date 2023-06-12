5f893be38a5fc4df5c30ee6cefab0b12043f99ab
sergiocorato <sergiocorato@gmail.com>
2023-06-12 17:22:40 +0200

    [FIX] license

--- account_invoice_dueamount/tests/test_account_invoice_dueamount.py
+++ account_invoice_dueamount/tests/test_account_invoice_dueamount.py
@@ -1,5 +1,5 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields
 from odoo.tests import tagged
