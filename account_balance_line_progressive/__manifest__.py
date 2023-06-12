8f2b58b5fa0c477e00708e258620e134406ede8b
sergiocorato <sergiocorato@gmail.com>
2023-06-12 17:22:49 +0200

    [FIX] license

--- account_balance_line_progressive/__manifest__.py
+++ account_balance_line_progressive/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2023 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account balance line progressive",
     "version": "14.0.1.0.0",
