ef14c1bb3d6c46d449deed996d50d1e4800e5654
sergiocorato <sergiocorato@gmail.com>
2023-06-12 17:40:47 +0200

    [MIG] account_invoice_line_usability: Migration to 14.0

--- account_invoice_line_usability/models/account_invoice_line.py
+++ account_invoice_line_usability/models/account_invoice_line.py
@@ -5,23 +5,25 @@ from odoo import api, fields, models
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = "account.move.line"
 
-    account_code = fields.Char(
-        related='account_id.code')
-    product_standard_price = fields.Float(
-        related='product_id.standard_price')
+    account_code = fields.Char(related="account_id.code")
+    product_standard_price = fields.Float(related="product_id.standard_price")
     move_price_unit = fields.Float(
-        string="Move Price Unit", compute='_get_move_price_unit', store=True)
+        string="Move Price Unit", compute="_compute_move_price_unit", store=True
+    )
     move_price_total = fields.Float(
-        string="Price Total", compute='_get_move_price_unit', store=True)
+        string="Price Total", compute="_compute_move_price_unit", store=True
+    )
 
-    @api.multi
-    @api.depends('move_line_ids.price_unit')
-    def _get_move_price_unit(self):
+    @api.depends("move_line_ids.price_unit")
+    def _compute_move_price_unit(self):
         for line in self:
             line.move_price_unit = min(
-                [-abs(x.price_unit) for x in line.move_line_ids] or [0])
+                [-abs(x.price_unit) for x in line.move_line_ids] or [0]
+            )
             line.move_price_total = line.quantity * (
-                line.move_price_unit if line.move_price_unit != 0.0 else
-                - line.product_standard_price)
+                line.move_price_unit
+                if line.move_price_unit != 0.0
+                else -line.product_standard_price
+            )
