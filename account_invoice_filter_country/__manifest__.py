37844bf92d4b0ba9717588d0bac6727e8018888b
sergiocorato <sergiocorato@gmail.com>
2023-06-12 17:22:22 +0200

    [IMP] account_invoice_filter_country: black, isort, prettier

--- account_invoice_filter_country/__manifest__.py
+++ account_invoice_filter_country/__manifest__.py
@@ -1,18 +1,16 @@
 # Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Invoice filter country',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'Add country field to filter in invoice.',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Invoice filter country",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
