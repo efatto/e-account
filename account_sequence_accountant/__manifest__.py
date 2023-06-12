73d54cda1f2967b30639e5111d232e1b4900d599
sergiocorato <sergiocorato@gmail.com>
2023-06-12 11:13:05 +0200

    [IMP] account_sequence_accountant: black, isort, prettier

--- account_sequence_accountant/__manifest__.py
+++ account_sequence_accountant/__manifest__.py
@@ -1,18 +1,17 @@
 # Copyright 2017-2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sequence access to accountant',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'description': 'Add ability to modify sequence from accountant users',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Sequence access to accountant",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account_move_name_sequence",
     ],
-    'data': [
-        'security/ir.model.access.csv',
+    "data": [
+        "security/ir.model.access.csv",
     ],
-    'installable': True,
+    "installable": True,
 }
