70ed205816d359a7c53949a308cd022e52a9b650
sergiocorato <sergiocorato@gmail.com>
2023-06-15 15:09:25 +0200

    [IMP] res_partner_view_account: black, isort, prettier

--- res_partner_view_account/__manifest__.py
+++ res_partner_view_account/__manifest__.py
@@ -3,19 +3,19 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Partner button view account moves',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'author': 'Alexis de Lattre - Akretion, Sergio Corato',
-    'description': 'With this module partner will have button with '
-                   'only account moves linked to account.',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Partner button view account moves",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Alexis de Lattre - Akretion, Sergio Corato",
+    "summary": "This module add button to open credit or debit account moves "
+    "related to the partner.",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/partner_account_view.xml',
+    "data": [
+        "views/partner_account_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
