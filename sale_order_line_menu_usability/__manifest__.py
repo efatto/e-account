2280297059ea142b3ccc7711c813880d1767abf3
sergiocorato <sergiocorato@gmail.com>
2023-06-15 16:31:15 +0200

    [IMP] sale_order_line_menu_usability: black, isort, prettier [MIG] sale_order_line_menu_usability: Migration to 14.0

--- sale_order_line_menu_usability/__manifest__.py
+++ sale_order_line_menu_usability/__manifest__.py
@@ -1,19 +1,19 @@
-# Copyright 2017-2022 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2017-2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale order line menu usability',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'This module extend menu for uninvoiced order lines.',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale_backorder',
-        'sale_order_line_date',
+    "name": "Sale order line menu usability",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "This module extend menu for uninvoiced order lines.",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_backorder",
+        "sale_order_line_date",
     ],
-    'data': [
-        'views/sale.xml',
+    "data": [
+        "views/sale.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
