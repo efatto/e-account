39e9cc992f89168c54db135d2c94fef6b8a670c2
sergiocorato <sergiocorato@gmail.com>
2023-06-15 16:19:53 +0200

    [IMP] sale_order_date_order_visibility: black, isort, prettier

--- sale_order_date_order_visibility/__manifest__.py
+++ sale_order_date_order_visibility/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "Sale order date visibility",
+    "name": "Sale order project, analytic and opportunity visibility",
     "version": "14.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Move date_order for quotation in header for better visibility "
-    "with opportunity, client order ref, project and analytic.",
+    "summary": "Move project, opportunity and analytic for quotation in header.",
     "website": "https://github.com/sergiocorato/e-account",
     "license": "AGPL-3",
     "depends": [
         "sale_crm",
+        "sale_date_order_visibility",
         "sale_timesheet",
     ],
     "data": [
