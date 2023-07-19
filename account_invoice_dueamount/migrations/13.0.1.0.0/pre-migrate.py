51b451c44c0c5287d0ed59cbb1c74d8acdbe1462
sergiocorato <sergiocorato@gmail.com>
2023-07-19 14:46:28 +0200

    [FIX] migrate

--- account_invoice_dueamount/migrations/13.0.1.0.0/pre-migrate.py
+++ account_invoice_dueamount/migrations/13.0.1.0.0/pre-migrate.py
@@ -5,39 +5,37 @@ from openupgradelib import openupgrade
 from psycopg2 import sql
 
 
-@openupgrade.migrate()
-def migrate(env, version):
+def migrate(cr, version):
+    drop_sql = sql.SQL("ALTER TABLE {} DROP CONSTRAINT {}")
     table = "account_invoice_dueamount_line"
     column = "invoice_id"
-    if openupgrade.column_exists(env.cr, "account_move", "old_invoice_id"):
-        # remove constraint for invoice_id
-        drop_sql = sql.SQL("ALTER TABLE {} DROP CONSTRAINT {}")
-        env.cr.execute(
-            """
-                SELECT constraint_name
-                FROM information_schema.table_constraints
-                WHERE constraint_type = 'FOREIGN KEY' AND table_name = %s
-                AND constraint_name like %s
-            """,
-            (table, "%%%s%%" % column),
-        )
-        for constraint in (row[0] for row in env.cr.fetchall()):
-            openupgrade.logged_query(
-                env.cr,
-                drop_sql.format(
-                    sql.Identifier(table),
-                    sql.Identifier(constraint),
-                ),
-            )
+    cr.execute(
+        """
+            SELECT constraint_name
+            FROM information_schema.table_constraints
+            WHERE constraint_type = 'FOREIGN KEY' AND table_name = %s
+            AND constraint_name like %s
+        """,
+        (table, "%%%s%%" % column),
+    )
+    for constraint in (row[0] for row in cr.fetchall()):
         openupgrade.logged_query(
-            env.cr,
-            sql.SQL(
-                """UPDATE {0} t
-            SET {1} = m.id
-            FROM account_move m
-            WHERE m.old_invoice_id = t.{1}"""
-            ).format(
+            cr,
+            drop_sql.format(
                 sql.Identifier(table),
-                sql.Identifier(column),
+                sql.Identifier(constraint),
             ),
+        )
+    openupgrade.logged_query(
+        cr,
+        sql.SQL(
+            """UPDATE {0} t
+        SET {1} = am.id
+        from account_invoice inv
+        join account_move am on am.id = inv.move_id
+        WHERE t.{1} = inv.id"""
+        ).format(
+            sql.Identifier(table),
+            sql.Identifier(column),
+        ),
         )
