c9c840837815d3e1b20cbf4be75c0e7e5511b1a1
sergiocorato <sergiocorato@gmail.com>
2023-09-01 16:52:16 +0200

    [MIG] mail_no_autofollow: Migration to 14.0

--- mail_no_autofollow/models/mail_thread.py
+++ mail_no_autofollow/models/mail_thread.py
@@ -11,16 +11,15 @@ class MailThread(models.AbstractModel):
             MailThread, self.with_context(mail_create_nosubscribe=True)
         ).create(vals_list=vals_list)
 
-    @api.multi
-    @api.returns("self", lambda value: value.id)
-    def message_post(self, *args, **kwargs):
+    @api.returns("mail.message", lambda value: value.id)
+    def message_post(self, **kwargs):
         """Force to no autofollow as in many models (sale.order, purchase.order, ...) it
         is set to True by default.
         """
         mail_post_autofollow = False
         return super(
             MailThread, self.with_context(mail_post_autofollow=mail_post_autofollow)
-        ).message_post(*args, **kwargs)
+        ).message_post(**kwargs)
 
     def _message_auto_subscribe_followers(self, updated_values, default_subtype_ids):
         # Remove auto-subscribe by default
