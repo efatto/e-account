6783f3de6f11368c98234ec159e3a5d54924cb8e
sergiocorato <sergiocorato@gmail.com>
2023-09-01 16:52:16 +0200

    [MIG] mail_no_autofollow: Migration to 14.0

--- mail_no_autofollow/wizard/mail_compose_message.py
+++ mail_no_autofollow/wizard/mail_compose_message.py
@@ -1,13 +1,11 @@
-from odoo import api, models
+from odoo import models
 
 
 class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
-    @api.multi
     def send_mail(self, auto_commit=False):
         for wizard in self:
             super(
                 MailComposeMessage, wizard.with_context(mail_post_autofollow=False)
             ).send_mail(auto_commit=auto_commit)
-        return True
