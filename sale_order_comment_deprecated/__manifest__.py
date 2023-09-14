acfb793413b2824e85635e27af4b6e7f797eb767
sergiocorato <sergiocorato@gmail.com>
2023-09-14 10:14:01 +0200

    [FIX] module name

--- sale_order_comment_deprecated/__manifest__.py
+++ sale_order_comment_deprecated/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Sale date deprecated comment preserve",
+    "name": "Sale order deprecated comment preserve",
     "version": "14.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
