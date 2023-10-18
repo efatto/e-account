4e5c49ee54c08e51f3a7cbafd5dc86ed637908ba
sergiocorato <sergiocorato@gmail.com>
2023-10-18 11:33:18 +0200

    [IMP] sale_order_view_usability: black, isort, prettier and Migration to 14.0

--- sale_order_view_usability/__manifest__.py
+++ sale_order_view_usability/__manifest__.py
@@ -1,23 +1,20 @@
 # Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Sale order view usability',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': '''
-This module extend sale order views with:
------------------------------------------
-* add sale order ref, origin and total amount in tree view,
-* add sale order ref in form view,
-* show origin field to base user.''',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'LGPL-3',
-    'depends': [
-        'sale',
+    "name": "Sale order view usability",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Extend sale order views adding sale order ref, origin and total amount"
+    " in tree view, sale order ref in form view, "
+    "show origin field to base user.",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
     ],
-    'data': [
-        'views/sale_order_view.xml',
+    "data": [
+        "views/sale_order_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
