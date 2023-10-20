4327901d3bf6037033fa181927eb9d0bc58225fc
sergiocorato <sergiocorato@gmail.com>
2023-10-20 15:53:04 +0200

    [IMP] sale_order_margin_state: black, isort, prettier and Migration to 14.0

--- sale_order_margin_state/models/res_config_settings.py
+++ sale_order_margin_state/models/res_config_settings.py
@@ -4,32 +4,32 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     margin_min = fields.Float(
         string="Minimum margin on order line",
         default=40.0,
-        help="An order line with a margin below this value will be signaled in red."
+        help="An order line with a margin below this value will be signaled in red.",
     )
     margin_max = fields.Float(
         string="Maximum margin on order line",
         default=100.0,
-        help="An order line with a margin over this value will be signaled in grey."
+        help="An order line with a margin over this value will be signaled in grey.",
     )
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     margin_min = fields.Float(
-        related='company_id.margin_min',
+        related="company_id.margin_min",
         string="Minimum margin on order line",
         help="An order line with a margin below this value will be signaled in red.",
-        readonly=False
+        readonly=False,
     )
     margin_max = fields.Float(
-        related='company_id.margin_max',
+        related="company_id.margin_max",
         string="Maximum margin on order line",
         help="An order line with a margin over this value will be signaled in grey.",
-        readonly=False
+        readonly=False,
     )
