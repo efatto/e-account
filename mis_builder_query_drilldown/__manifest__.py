10b0620a0d8deac563c24f9adaba9fe1a5be2963
sergiocorato <sergiocorato@gmail.com>
2023-10-25 12:48:51 +0200

    [IMP] mis_builder_query_drilldown: black, isort, prettier and Migration to 14.0

--- mis_builder_query_drilldown/__manifest__.py
+++ mis_builder_query_drilldown/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Mis builder drilldown query",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Reporting",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-account",
