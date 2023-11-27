3992c6f6d9cdb0e8f3b5f721b37097da82f29cdf
sergiocorato <sergiocorato@gmail.com>
2023-11-27 08:55:17 +0100

    [ADD] calendar view to timesheets

---
+++ account_analytic_line_calendar/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Add calendar view to analytic line",
+    "version": "14.0.1.0.0",
+    "category": "Localisation",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_timesheet",
+        "project_timesheet_time_control",
+    ],
+    "data": [
+        "views/hr_timesheet_view.xml",
+    ],
+    "installable": True,
+}
