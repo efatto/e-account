1e113c17e57528792c600b2e5283297696652470
sergiocorato <sergiocorato@gmail.com>
2023-11-28 16:51:38 +0100

    [ADD] module to preserve date_order

---
+++ sale_order_confirmation_date/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale order confirmation date",
+    "version": "14.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
