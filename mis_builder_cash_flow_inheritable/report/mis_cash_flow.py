651070d62be3548b1ebc867c7e732cfb4f5859e6
sergiocorato <sergiocorato@gmail.com>
2024-01-03 16:11:57 +0100

    [IMP] mis_builder_cash_flow_inheritable: black, isort, prettier and Migration to 14.0

--- mis_builder_cash_flow_inheritable/report/mis_cash_flow.py
+++ mis_builder_cash_flow_inheritable/report/mis_cash_flow.py
@@ -1,24 +1,23 @@
 # Copyright 2019 ADHOC SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, tools
 from psycopg2.extensions import AsIs
+
+from odoo import fields, models, tools
 
 
 class MisCashFlow(models.Model):
-    _inherit = 'mis.cash_flow'
+    _inherit = "mis.cash_flow"
 
     invoiced_percent = fields.Float()
-    currency_id = fields.Many2one(
-        comodel_name='res.currency'
-    )
+    currency_id = fields.Many2one(comodel_name="res.currency")
     res_model = fields.Char()
     balance_currency = fields.Float()
     balance_forecast = fields.Float()
 
     def get_cash_flow_query(self):
-        account_type_receivable = self.env.ref(
-            'account.data_account_type_receivable')
-        query = """
+        account_type_receivable = self.env.ref("account.data_account_type_receivable")
+        query = (
+            """
             SELECT
                 -- we use negative id to avoid duplicates and we don't use
                 -- ROW_NUMBER() because the performance was very poor
@@ -40,7 +39,7 @@ class MisCashFlow(models.Model):
                 aml.full_reconcile_id as full_reconcile_id,
                 aml.partner_id as partner_id,
                 aml.company_id as company_id,
-                aml.user_type_id as user_type_id,
+                aa.user_type_id as user_type_id,
                 aml.name as name,
                 aml.date_maturity as date,
                 CAST('account_move_line' as varchar) as res_model,
@@ -50,6 +49,7 @@ class MisCashFlow(models.Model):
                 0.0 as balance_currency,
                 0.0 as balance_forecast
             FROM account_move_line as aml
+            LEFT JOIN account_account as aa ON aa.id = aml.account_id
             UNION ALL
             SELECT
                 fl.id as id,
@@ -84,14 +84,14 @@ class MisCashFlow(models.Model):
                 ir_model im ON im.id = fl.res_model_id
             WHERE
                 fl.res_model_id IS NULL
-        """ % account_type_receivable.id
+        """
+            % account_type_receivable.id
+        )
         return query
 
-    @api.model_cr
     def init(self):
         query = self.get_cash_flow_query()
         tools.drop_view_if_exists(self.env.cr, self._table)
-        self._cr.execute(
-            'CREATE OR REPLACE VIEW %s as %s',
-            (AsIs(self._table), AsIs(query))
+        self.env.cr.execute(
+            "CREATE OR REPLACE VIEW %s as %s", (AsIs(self._table), AsIs(query))
         )
