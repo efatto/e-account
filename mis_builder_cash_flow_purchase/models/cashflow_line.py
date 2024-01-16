0ac75615b25b8aac5a034468625cdf11e47d88b4
sergiocorato <sergiocorato@gmail.com>
2024-01-16 15:51:59 +0100

    [FIX] align to new mis_builder cashflow line - some refactor

--- mis_builder_cash_flow_purchase/models/cashflow_line.py
+++ mis_builder_cash_flow_purchase/models/cashflow_line.py
@@ -16,11 +16,11 @@ class CashFlowForecastLine(models.Model):
         help="Purchase amount in vendor currency recomputed with delivered qty",
     )
     purchase_invoiced_percent = fields.Float(
-        compute="_compute_balance_forecast", store=True
+        compute="_compute_purchase_balance_forecast", store=True
     )
     purchase_balance_forecast = fields.Float(
-        compute="_compute_balance_forecast",
-        string="Forecast balance",
+        compute="_compute_purchase_balance_forecast",
+        string="Purchase forecast balance",
         store=True,
     )
 
@@ -34,18 +34,17 @@ class CashFlowForecastLine(models.Model):
         "purchase_line_id.order_id.date_order",
         "purchase_line_id.order_id.currency_id.rate",
     )
-    def _compute_balance_forecast(self):
+    def _compute_purchase_balance_forecast(self):
         for line in self:
             if line.purchase_line_id:
-                line.purchase_invoiced_percent = line.purchase_line_id.qty_invoiced / (
+                purchase_invoiced_percent = line.purchase_line_id.qty_invoiced / (
                     max(
-                        [
-                            line.purchase_line_id.product_qty,
-                            line.purchase_line_id.qty_received,
-                            1,
-                        ]
+                        line.purchase_line_id.product_qty,
+                        line.purchase_line_id.qty_received,
+                        1,
                     )
                 )
+                line.purchase_invoiced_percent = min(purchase_invoiced_percent, 1)
                 line.purchase_balance_forecast = -line.currency_id._convert(
                     line.purchase_balance_currency or line.balance,
                     line.purchase_line_id.order_id.company_id.currency_id,
