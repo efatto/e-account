57655a0b174885d6266a8b06df0248f0872f818d
sergiocorato <sergiocorato@gmail.com>
2024-02-12 18:24:47 +0100

    [ADD] module for email_shipping

---
+++ res_partner_email_shipping/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Additional mail shipping for partner',
+    'version': '14.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'summary': 'A simple mail used for shipping.',
+    'website': 'https://github.com/sergiocorato/e-account',
+    'license': 'AGPL-3',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'views/partner.xml',
+    ],
+    'installable': True
+}
