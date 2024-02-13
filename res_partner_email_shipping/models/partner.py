0941d9cb6d325d7153645c00a6e0976bb742a524
sergiocorato <sergiocorato@gmail.com>
2024-02-13 10:31:16 +0100

    [IMP] add email template

--- res_partner_email_shipping/models/partner.py
+++ res_partner_email_shipping/models/partner.py
@@ -1,28 +1,11 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    email_shipping = fields.Char(string='Email Shipping')
-
-    def action_do_send_email(self):
-        """Opens a wizard to compose an email"""
-        self.ensure_one()
-        self.lang or self.env.context.get("lang")
-        ctx = {
-            "default_model": "res.partner",
-            "default_res_id": self.ids[0],
-            "default_composition_mode": "comment",
-            "default_partner_ids": [self.id],
-            "force_email": True,
-        }
-        return {
-            "type": "ir.actions.act_window",
-            "view_mode": "form",
-            "res_model": "mail.compose.message",
-            "views": [(False, "form")],
-            "view_id": False,
-            "target": "new",
-            "context": ctx,
-        }
+    email_shipping = fields.Char(string="Email Shipping")
+    email_shipping_template_id = fields.Many2one(
+        comodel_name="mail.template",
+        string="Shipping Email Template",
+    )
