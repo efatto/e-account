9c8ee9f15f766a562108ff6e18eba2ced3bec2e9
sergiocorato <sergiocorato@gmail.com>
2024-02-14 11:47:55 +0100

    [FIX] email values

---
+++ account_invoice_section_sale_order_single/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account invoice force group for one single sale order",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "This module force adding the group with one single sale order too",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_section_sale_order",
+    ],
+    "data": [],
+    "installable": True,
+}
