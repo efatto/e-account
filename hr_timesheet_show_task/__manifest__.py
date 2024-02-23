a340243f22f9ffe7305d646db906c3fb92a70419
sergiocorato <sergiocorato@gmail.com>
2024-02-23 18:10:48 +0100

    [ADD] modulo estremamente complesso per soddisfare le esigenze dei clienti più esigenti

---
+++ hr_timesheet_show_task/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Show task in timesheet",
+    "version": "14.0.1.0.0",
+    "category": "Timesheet",
+    "license": "AGPL-3",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "depends": [
+        "hr_timesheet",
+    ],
+    "data": [
+        "views/hr_timesheet.xml",
+    ],
+    "installable": True,
+}
