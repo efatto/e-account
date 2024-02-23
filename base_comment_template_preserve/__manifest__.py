fa3276285337ad476984ee6a70bf04eb840484b4
sergiocorato <sergiocorato@gmail.com>
2024-02-23 12:07:39 +0100

    [FIX] rename

--- base_comment_template_preserve/__manifest__.py
+++ base_comment_template_preserve/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Base comment preserve comments",
+    "name": "Base comment template preserve",
     "version": "14.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
