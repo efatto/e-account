df1698ed846abf8f52f22a94279cd3f8c0d8fb2e
sergiocorato <sergiocorato@gmail.com>
2024-02-23 11:52:26 +0100

    [ADD] module to preserve present comment

---
+++ base_comment_template_preserve_default/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Base comment preserve comments",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "summary": "This module preserves the comments already present in the "
+    "document if they are not defined in the partner.",
+    "depends": [
+        "base_comment_template",
+    ],
+    "data": [],
+    "installable": True,
+}
