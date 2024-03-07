a2765c8c97c67418014b2427ee332c8ecb9ebb4b
sergiocorato <sergiocorato@gmail.com>
2024-03-07 14:45:23 +0100

    [FIX] pre-commit

--- sale_order_confirmation_date/report/sale_report.py
+++ sale_order_confirmation_date/report/sale_report.py
@@ -4,9 +4,7 @@ from odoo import fields, models
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    confirmation_date = fields.Datetime(
-        string="Confirmation date",
-        readonly=True)
+    confirmation_date = fields.Datetime(string="Confirmation date", readonly=True)
 
     def _select_additional_fields(self, add_fields):
         add_fields["confirmation_date"] = ", s.confirmation_date AS confirmation_date"
