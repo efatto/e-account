c01b75812b94805ed47ef77f90c5b48008bc31b6
sergiocorato <sergiocorato@gmail.com>
2024-05-22 11:01:43 +0200

    [FIX] account_invoice_section_sale_order_single super() method

--- account_invoice_section_sale_order_single/models/sale_order.py
+++ account_invoice_section_sale_order_single/models/sale_order.py
@@ -7,7 +7,7 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _create_invoices(self, grouped=False, final=False, date=None):
-        invoice_ids = super()._create_invoices(grouped, final, date)
+        invoice_ids = super()._create_invoices(grouped=grouped, final=final, date=date)
         for invoice in invoice_ids:
             if (
                 len(invoice.line_ids.mapped(invoice.line_ids._get_section_grouping()))
