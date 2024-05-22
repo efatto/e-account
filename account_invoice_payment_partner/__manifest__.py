5cc04ce357bfb7f7071d9ccc6f9d0cd112829ae7
sergiocorato <sergiocorato@gmail.com>
2024-05-22 09:55:11 +0200

    [IMP] account_invoice_payment_partner: black, isort, prettier and Migration to 14.0

--- account_invoice_payment_partner/__manifest__.py
+++ account_invoice_payment_partner/__manifest__.py
@@ -1,18 +1,17 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account invoice payment partner',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'summary': 'This module syncronize the partner bank of the invoice on the payment '
-               'mode if with a single bank.',
-    'website': 'https://github.com/sergiocorato/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_payment_partner',
+    "name": "Account invoice payment partner",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "This module syncronize the partner bank of the invoice on the payment "
+    "mode if with a fixed bank.",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_partner",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
--- account_invoice_payment_partner/models/account_invoice.py
+++ account_invoice_payment_partner/models/account_invoice.py
@@ -1,17 +1,17 @@
-from odoo import models, api
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
-    @api.onchange('payment_mode_id')
+    @api.onchange("payment_mode_id")
     def _onchange_payment_mode_id(self):
         super()._onchange_payment_mode_id()
         pay_mode = self.payment_mode_id
         if (
-            pay_mode and
-            pay_mode.payment_type == 'inbound' and
-            pay_mode.bank_account_link == 'fixed' and
-            pay_mode.fixed_journal_id.bank_account_id
+            pay_mode
+            and pay_mode.payment_type == "inbound"
+            and pay_mode.bank_account_link == "fixed"
+            and pay_mode.fixed_journal_id.bank_account_id
         ):
             self.partner_bank_id = pay_mode.fixed_journal_id.bank_account_id
