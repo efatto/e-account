3f73407bfdce7d3fb4889234b27733ae815e6c9f
sergiocorato <sergiocorato@gmail.com>
2024-06-06 17:43:53 +0200

    [ADD] module for intrastat readonly in invoice

---
+++ account_invoice_intrastat_readonly/models/account_invoice.py
@@ -0,0 +1,12 @@
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.move"
+
+    @api.onchange("intrastat")
+    def onchange_instrastat(self):
+        if self.fiscal_position_id.intrastat != self.intrastat:
+            raise ValidationError(_("Intrastat is linked from fiscal position!\n"
+                                    "Change fiscal position to set it."))
