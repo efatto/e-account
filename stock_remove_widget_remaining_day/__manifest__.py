6cfc733338d8e92714fb0e585ff18371e7f560d0
sergiocorato <sergiocorato@gmail.com>
2024-06-06 09:42:02 +0200

    [IMP] remove widget remaining days from mrp

--- stock_remove_widget_remaining_day/__manifest__.py
+++ stock_remove_widget_remaining_day/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Remove widget remaining days in picking",
-    "version": "14.0.1.0.0",
+    "name": "Remove widget remaining days in picking and MRP",
+    "version": "14.0.1.0.1",
     "category": "Stock Management",
     "license": "AGPL-3",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-account",
     "depends": [
+        "mrp",
         "stock",
     ],
     "data": [
