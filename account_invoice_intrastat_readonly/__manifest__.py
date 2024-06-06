546d84de981917e3f4837286aa6a99dcf9a1b5fd
sergiocorato <sergiocorato@gmail.com>
2024-06-06 17:43:53 +0200

    [ADD] module for intrastat readonly in invoice

---
+++ account_invoice_intrastat_readonly/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Intrastat readonly in account invoice",
+    "version": "14.0.1.0.0",
+    "category": "Localisation",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_intrastat",
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}
---
+++ account_invoice_intrastat_readonly/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module block user to change intrastat flag in the invoice.
