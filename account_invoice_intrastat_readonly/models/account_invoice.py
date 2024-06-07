c81ab141676dc32d9db75a9a77852e2c095dcaa8
sergiocorato <sergiocorato@gmail.com>
2024-06-07 10:44:57 +0200

    [FIX] pre-commit

--- account_invoice_intrastat_readonly/models/account_invoice.py
+++ account_invoice_intrastat_readonly/models/account_invoice.py
@@ -1,4 +1,4 @@
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
@@ -8,5 +8,9 @@ class AccountInvoice(models.Model):
     @api.onchange("intrastat")
     def onchange_instrastat(self):
         if self.fiscal_position_id.intrastat != self.intrastat:
-            raise ValidationError(_("Intrastat is linked from fiscal position!\n"
-                                    "Change fiscal position to set it."))
+            raise ValidationError(
+                _(
+                    "Intrastat is linked from fiscal position!\n"
+                    "Change fiscal position to set it."
+                )
+            )
