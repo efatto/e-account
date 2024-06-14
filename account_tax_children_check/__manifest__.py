5dada0dca7cd93422a74db656d60ac7fff660403
sergiocorato <sergiocorato@gmail.com>
2024-06-14 17:58:45 +0200

    [FIX] version

--- account_tax_children_check/__manifest__.py
+++ account_tax_children_check/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Tax children check",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-account",
     "category": "Accounting",
