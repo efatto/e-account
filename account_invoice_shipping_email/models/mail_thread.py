25c7a013824efaac2d4058a53b71deb9fe3a4f9e
sergiocorato <sergiocorato@gmail.com>
2024-06-22 08:17:46 +0200

    [IMP] force reply-to

---
+++ account_invoice_shipping_email/models/mail_thread.py
@@ -0,0 +1,13 @@
+
+from odoo import api, models
+
+
+class MailThread(models.AbstractModel):
+    _inherit = "mail.thread"
+
+    @api.returns("mail.message", lambda value: value.id)
+    def message_post(self, **kwargs):
+        """Force reply_to as it is overriden by the catchall."""
+        if self.env.context.get("default_reply_to", False):
+            kwargs["reply_to"] = self.env.context.get("default_reply_to")
+        return super().message_post(**kwargs)
