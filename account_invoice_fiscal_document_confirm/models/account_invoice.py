919faa20377e610fa75c29b90443e11f6ac19965
sergiocorato <sergiocorato@gmail.com>
2024-07-12 15:11:53 +0200

    [FIX] pre-commit

--- account_invoice_fiscal_document_confirm/models/account_invoice.py
+++ account_invoice_fiscal_document_confirm/models/account_invoice.py
@@ -1,4 +1,4 @@
-from odoo import _, api, models, fields
+from odoo import api, fields, models
 from odoo.fields import first
 
 
@@ -13,13 +13,10 @@ class AccountInvoice(models.Model):
     @api.depends("fiscal_document_type_id", "move_type")
     def _compute_is_fiscal_document_type_not_default(self):
         default_fiscal_document_type_id = first(
-            self.env["fiscal.document.type"].search(
-                [("out_invoice", "=", True)]
-            )
+            self.env["fiscal.document.type"].search([("out_invoice", "=", True)])
         )
         for invoice in self:
             self.is_fiscal_document_type_default = (
                 invoice.move_type == "out_invoice"
-                and invoice.fiscal_document_type_id
-                == default_fiscal_document_type_id
+                and invoice.fiscal_document_type_id == default_fiscal_document_type_id
             )
