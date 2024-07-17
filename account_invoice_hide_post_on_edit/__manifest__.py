3059336288e068447d8e12b26719d078cbbd9ca4
sergiocorato <sergiocorato@gmail.com>
2024-07-17 08:36:19 +0200

    [IMP] rename module account invoice confirmation to hide post

--- account_invoice_hide_post_on_edit/__manifest__.py
+++ account_invoice_hide_post_on_edit/__manifest__.py
@@ -1,18 +1,17 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Account invoice request confirm on fiscal document",
-    "version": "14.0.1.0.0",
+    "name": "Account invoice hide confirm button on edit",
+    "version": "14.0.1.0.1",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": """
-    Add confirm request if out invoice fiscal document type is not the default one.
+    Hide Confirm button on editing account move.
     """,
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-account",
     "depends": [
         "account",
-        "l10n_it_fiscal_document_type",
     ],
     "data": [
         "views/account_invoice_view.xml",
