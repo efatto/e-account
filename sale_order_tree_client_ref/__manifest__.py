fec46d54753932fbe23ff407af025ed276aca4dc
sergiocorato <sergiocorato@gmail.com>
2024-07-18 15:05:32 +0200

    [IMP] sale description in product tree view

--- sale_order_tree_client_ref/__manifest__.py
+++ sale_order_tree_client_ref/__manifest__.py
@@ -1,17 +1,19 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale order tree view client ref",
+    "name": "Sale order tree and product",
     "version": "14.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Add sale order ref in tree view",
+    "summary": "Add sale order ref in tree view and sale description in product tree "
+    "view.",
     "website": "https://github.com/sergiocorato/e-account",
     "license": "AGPL-3",
     "depends": [
         "sale",
     ],
     "data": [
+        "views/product_view.xml",
         "views/sale_order_view.xml",
     ],
     "installable": True,
