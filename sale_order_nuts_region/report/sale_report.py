4bc71c92c9e0b0f1438704e85ffeb6b75ea9dd89
sergiocorato <sergiocorato@gmail.com>
2024-07-22 15:06:53 +0200

    [ADD] module to add region field to sale

---
+++ sale_order_nuts_region/report/sale_report.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    nuts3_id = fields.Many2one(
+        comodel_name="res.partner.nuts",
+        string="Region",
+        readonly=True,
+    )
+
+    def _select_additional_fields(self, add_fields):
+        add_fields["nuts3_id"] = ", partner.nuts3_id AS nuts3_id"
+        return super()._select_additional_fields(add_fields)
+
+    def _group_by_sale(self, groupby=""):
+        groupby_ = super()._group_by_sale(groupby=groupby)
+        groupby_ += " , partner.nuts3_id"
+        return groupby_
