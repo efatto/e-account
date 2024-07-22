a4e7a0fe4e596ee9428416c0cf02232c686bc9d6
sergiocorato <sergiocorato@gmail.com>
2024-07-22 15:06:53 +0200

    [ADD] module to add region field to sale

---
+++ sale_order_nuts_region/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale order and report Region info",
+    "version": "14.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "base_location_nuts",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
