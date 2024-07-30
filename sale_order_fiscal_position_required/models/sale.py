97310689283a9f8acf557ae8e41c3aa2a7284b90
sergiocorato <sergiocorato@gmail.com>
2024-07-30 09:32:39 +0200

    [FIX] add ability to deactivate requirement on fiscal position

--- sale_order_fiscal_position_required/models/sale.py
+++ sale_order_fiscal_position_required/models/sale.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, models, _
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 from odoo.tools import config
 
@@ -8,12 +8,20 @@ from odoo.tools import config
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    is_fiscal_position_required = fields.Boolean(
+        string='Is Fiscal Position Required',
+        default=True,
+    )
+
     @api.multi
     def action_confirm(self):
-        if not config["test_enable"] or self.env.context.get(
-            "test_sale_order_fiscal_position_required"
-        ):
-            if any(self.filtered(lambda so: not so.fiscal_position_id)):
-                raise ValidationError(_("Missing fiscal position!"))
+        if not config["test_enable"]:
+            if any(
+                self.filtered(
+                    lambda so: so.is_fiscal_position_required
+                    and not so.fiscal_position_id
+                )
+            ):
+                raise ValidationError(_("Missing fiscal position in sale order!"))
         res = super().action_confirm()
         return res
