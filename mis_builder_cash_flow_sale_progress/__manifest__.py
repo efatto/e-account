5492e38b438f6dbcf407dad16394670c9dc51392
sergiocorato <sergiocorato@gmail.com>
2024-07-30 10:56:52 +0200

    [IMP] bump version modules cash flow and sale order progress

--- mis_builder_cash_flow_sale_progress/__manifest__.py
+++ mis_builder_cash_flow_sale_progress/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "MIS Builder sale progress cash flow",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Generate automatically cash flow lines from sale order progress.",
