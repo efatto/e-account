05354a4988466013d7f26a62cdd2748045bebc77
sergiocorato <sergiocorato@gmail.com>
2024-08-02 17:42:50 +0200

    [ADD] module account_invoice_reduced_price with price reduced compute

---
+++ account_invoice_reduced_price/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Invoice price unit net",
+    "summary": "Modulo che aggiunge nella riga fattura il prezzo al netto dello sconto",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Hidden",
+    "website": "https://github.com/sergiocorato/e-account",
+    "author": "Sergio Corato",
+    "maintainers": ["sergiocorato"],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "account_invoice_triple_discount",
+    ],
+    "data": [
+        "views/account_view.xml",
+    ],
+}
---
+++ account_invoice_reduced_price/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Questo modulo aggiunge nella riga fattura un campo prezzo al netto degli sconti applicati, per usi tecnici su altri moduli.
