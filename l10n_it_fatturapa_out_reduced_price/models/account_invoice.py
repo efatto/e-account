29c5a1dcf6561228a317ce9b142576a0d923fa52
sergiocorato <sergiocorato@gmail.com>
2024-08-05 12:02:15 +0200

    [IMP] remove related from invoice e-fattura export option - add readme

--- l10n_it_fatturapa_out_reduced_price/models/account_invoice.py
+++ l10n_it_fatturapa_out_reduced_price/models/account_invoice.py
@@ -5,7 +5,4 @@ from odoo import fields, models
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    export_invoice_reduced_price = fields.Boolean(
-        related="partner_id.export_invoice_reduced_price",
-        readonly=False,
-    )
+    export_invoice_reduced_price = fields.Boolean()
