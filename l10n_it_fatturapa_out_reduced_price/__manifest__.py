43757227e4bcc46e3b5f0e29b1fe2938f609ea56
sergiocorato <sergiocorato@gmail.com>
2024-08-05 09:04:56 +0200

    [FIX] module description

--- l10n_it_fatturapa_out_reduced_price/__manifest__.py
+++ l10n_it_fatturapa_out_reduced_price/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Invoice price unit net",
-    "summary": "Modulo che aggiunge la possibilità di stampare ed esportare i prezzi "
-               "al netto dello sconto, senza mostrare lo sconto",
+    "name": "E-invoice export with reduced price",
+    "summary": "This module add the ability to export the e-invoice "
+               "with the reduced price, hiding the discount",
     "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Hidden",
