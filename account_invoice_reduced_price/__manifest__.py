4ffe29030cdef140e625e760cc4357bab45b72f4
sergiocorato <sergiocorato@gmail.com>
2024-08-07 09:33:13 +0200

    [IMP] module desc

--- account_invoice_reduced_price/__manifest__.py
+++ account_invoice_reduced_price/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Invoice price unit net",
+    "name": "Account Invoice line reduced price",
     "summary": "Modulo che aggiunge nella riga fattura il prezzo al netto dello sconto",
     "version": "12.0.1.0.0",
     "development_status": "Beta",
