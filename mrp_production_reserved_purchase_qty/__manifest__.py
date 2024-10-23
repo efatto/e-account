23811253384499c1181964b99aa3c25904559312
sergiocorato <sergiocorato@gmail.com>
2024-10-23 09:28:44 +0200

    [IMP] description of method

--- mrp_production_reserved_purchase_qty/__manifest__.py
+++ mrp_production_reserved_purchase_qty/__manifest__.py
@@ -1,12 +1,14 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Mrp production reserve on PO Confirmed',
+    'name': 'Mrp production reserve on PO Confirmed or stock',
     'version': '12.0.1.0.0',
     'category': 'Manufacture',
     'license': 'AGPL-3',
     'summary': """
-    When a PO is confirmed, set reserved in related raw component production lines.
+    Add a purchase ordered qty when a PO is confirmed in production raw moves.
+    Set reserved in related raw component production lines on stock available or
+    incoming moves.
     """,
     'author': "Sergio Corato",
     'website': 'https://github.com/sergiocorato/e-account',
