e37c90e6cb43107868d87ef378a6a66161bf43e0
sergiocorato <sergiocorato@gmail.com>
2024-11-12 09:27:40 +0100

    [FIX] remove amount from intrastat info

--- account_invoice_info_intrastat/models/account_move.py
+++ account_invoice_info_intrastat/models/account_move.py
@@ -15,9 +15,8 @@ class AccountMove(models.Model):
             for line in move.invoice_line_ids.filtered(
                 lambda x: x.product_id and x.product_id.type != "service"
             ):
-                # if country of origin is not found in product,
-                # get it from first seller of the product, in the last chance from
-                # the current company
+                # if country of origin is not found in product, get from first seller
+                # of the product and eventually from the current company
                 country_name = (
                     line.product_id.intrastat_country_origin_id
                     and line.product_id.intrastat_country_origin_id.with_context(
@@ -41,16 +40,13 @@ class AccountMove(models.Model):
                 intrastat_text = f"\nHS CODE: {hs_code} "
                 intrastat_text += f"COUNTRY OF ORIGIN: {country_name} "
                 lang = get_lang(self.env, lang_code=self.env.company.partner_id.lang)
-                amount = lang.format(
-                    "%.2f", line.price_subtotal, grouping=True, monetary=True
-                )
                 weight = lang.format(
                     f"%.{precision_weight_digits or 2}f",
                     weight,
                     grouping=True,
                     monetary=False,
                 )
-                intrastat_text += f"NET WEIGHT: {weight} kg AMOUNT: € {amount}"
+                intrastat_text += f"NET WEIGHT: {weight} kg"
                 if "HS CODE" in line.name:
                     line.name = re.compile("\nHS CODE.*").sub("", line.name)
                 line.name += intrastat_text
@@ -58,7 +54,8 @@ class AccountMove(models.Model):
     def action_remove_intrastat_data_in_move_lines(self):
         for move in self.filtered(lambda x: x.move_type.startswith("out_")):
             for line in move.invoice_line_ids.filtered(
-                lambda x: x.product_id and x.product_id.type != "service"
+                lambda x: x.product_id
+                and x.product_id.type != "service"
+                and "HS CODE" in line.name
             ):
-                if "HS CODE" in line.name:
-                    line.name = re.compile("\nHS CODE.*").sub("", line.name)
+                line.name = re.compile("\nHS CODE.*").sub("", line.name)
