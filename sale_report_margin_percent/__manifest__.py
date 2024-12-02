b621ed2a0d6f12520304047220e57bc626adb289
sergiocorato <sergiocorato@gmail.com>
2024-12-02 12:18:42 +0100

    [FIX]  add weighted margin percent

--- sale_report_margin_percent/__manifest__.py
+++ sale_report_margin_percent/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale Report Margin Percent",
+    "name": "Sale Report Weighted Margin Percent",
     "version": "14.0.1.0.1",
     "category": "Sales",
     "author": "Sergio Corato",
-    "summary": "Adds the margin percent to sale analysis report",
+    "summary": "Adds the weighted margin percent to sale analysis report",
     "website": "https://github.com/sergiocorato/e-account",
     "license": "AGPL-3",
     "depends": ["sale_margin"],
--- sale_report_margin_percent/models/sale_order.py
+++ sale_report_margin_percent/models/sale_order.py
@@ -26,8 +26,11 @@ class SaleOrder(models.Model):
         )
         for order in res:
             if "__domain" in order and "margin_percent:avg" in fields:
-                orders = self.search(order["__domain"])
-                order["margin_percent"] = sum(orders.mapped("margin")) / (
-                    sum(orders.mapped("amount_untaxed")) or 1
+                orders = self.search_read(
+                    order["__domain"],
+                    ["margin", "amount_untaxed"]
                 )
+                order["margin_percent"] = sum([x["margin"] for x in orders]) / (
+                    sum([x["amount_untaxed"] for x in orders]) or 1
+                ) * 100.0
         return res
