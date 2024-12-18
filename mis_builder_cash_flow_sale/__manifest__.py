299361020d2e144b1cc72675107fe3df9a8ebc83
sergiocorato <sergiocorato@gmail.com>
2024-12-18 12:20:35 +0100

    [FIX] repo slug

--- mis_builder_cash_flow_sale/__manifest__.py
+++ mis_builder_cash_flow_sale/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Generate automatically cash flow lines from sale order line.",
-    "website": "https://github.com/sergiocorato/e-account",
+    "website": "https://github.com/efatto/e-account",
     "license": "AGPL-3",
     "depends": [
         "mis_builder_cash_flow_inheritable",
