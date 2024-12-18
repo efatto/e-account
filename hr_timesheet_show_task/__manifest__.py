cb1d407c93251eb50434b76e484d4c35b6da6f07
sergiocorato <sergiocorato@gmail.com>
2024-12-18 12:20:35 +0100

    [FIX] repo slug

--- hr_timesheet_show_task/__manifest__.py
+++ hr_timesheet_show_task/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Timesheet",
     "license": "AGPL-3",
     "author": "Sergio Corato",
-    "website": "https://github.com/sergiocorato/e-account",
+    "website": "https://github.com/efatto/e-account",
     "depends": [
         "hr_timesheet",
     ],
