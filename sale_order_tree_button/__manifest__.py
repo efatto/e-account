fc2e8de83b174f0246e031e6c4faa3a6a4d2c843
sergiocorato <sergiocorato@gmail.com>
2024-12-18 12:07:42 +0100

    [ADD] module sale order tree button

---
+++ sale_order_tree_button/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale order tree show buttons",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Add button to open opportunities, sales and tasks in tree view.",
+    "website": "https://github.com/sergiocorato/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "crm",
+        "sale_project",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
