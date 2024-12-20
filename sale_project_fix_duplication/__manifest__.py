a63cc36bf8697e54f4f22b0d217191bc9800b005
sergiocorato <sergiocorato@gmail.com>
2024-12-20 15:52:37 +0100

    [UPD] module desc

--- sale_project_fix_duplication/__manifest__.py
+++ sale_project_fix_duplication/__manifest__.py
@@ -1,8 +1,10 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale project fix duplication",
-    "summary": "Simple module to integrate PR https://github.com/OCA/OCB/pull/1186",
+    "name": "Sale project fix duplication DEPRECATED",
+    "summary": "Simple module to integrate PR https://github.com/OCA/OCB/pull/1186 "
+               "Update: this module create a bug in manual project creation, not "
+               "simply resolvable.",
     "version": "14.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/efatto/e-account",
