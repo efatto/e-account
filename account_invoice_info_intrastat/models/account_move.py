dc99442ce9bb4c80f1b08ff897becd466f533760
sergiocorato <sergiocorato@gmail.com>
2025-01-10 16:02:36 +0100

    [IMP] add summary intrastat info in narration

--- account_invoice_info_intrastat/models/account_move.py
+++ account_invoice_info_intrastat/models/account_move.py
@@ -7,11 +7,13 @@ from odoo.tools import float_round, get_lang
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def action_insert_intrastat_data_in_move_lines(self):
+    def action_insert_intrastat_data_invoice(self):
         precision_weight_digits = (
             self.env["decimal.precision"].search([("name", "=", "Stock Weight")]).digits
         )
         for move in self.filtered(lambda x: x.move_type.startswith("out_")):
+            intrastat_info = {}
+            lang = get_lang(self.env, lang_code=self.env.company.partner_id.lang)
             for line in move.invoice_line_ids.filtered(
                 lambda x: x.product_id and x.product_id.type != "service"
             ):
@@ -38,8 +40,10 @@ class AccountMove(models.Model):
                 if line.product_id.intrastat_code_id:
                     hs_code = line.product_id.intrastat_code_id.name
                 intrastat_text = f"\nHS CODE: {hs_code} "
+                if hs_code not in intrastat_info:
+                    intrastat_info[hs_code] = {"weight": 0, "price_subtotal": 0}
                 intrastat_text += f"COUNTRY OF ORIGIN: {country_name} "
-                lang = get_lang(self.env, lang_code=self.env.company.partner_id.lang)
+                intrastat_info[hs_code]["weight"] += weight
                 weight = lang.format(
                     f"%.{precision_weight_digits or 2}f",
                     weight,
@@ -47,11 +51,42 @@ class AccountMove(models.Model):
                     monetary=False,
                 )
                 intrastat_text += f"NET WEIGHT: {weight} kg"
+                intrastat_info[hs_code]["price_subtotal"] += line.price_subtotal
                 if "HS CODE" in line.name:
                     line.name = re.compile("\nHS CODE.*").sub("", line.name)
                 line.name += intrastat_text
+            if "HS Codes:" in move.narration:
+                move.narration = re.compile("\nHS Codes:.*").sub("", move.narration)
+                move.narration = re.compile("\n[0-9]{4,12}: .*€").sub(
+                    "", move.narration
+                )
+            move.narration += "\nHS Codes:\n"
+            intrastat_info = {
+                x: {
+                    "weight": lang.format(
+                        f"%.{precision_weight_digits or 2}f",
+                        intrastat_info[x]["weight"],
+                        grouping=True,
+                        monetary=False,
+                    ),
+                    "price_subtotal": lang.format(
+                        "%.2f",
+                        intrastat_info[x]["price_subtotal"],
+                        grouping=True,
+                        monetary=True,
+                    ),
+                }
+                for x in intrastat_info
+            }
+            move.narration += "\n".join(
+                [
+                    f"{n}: {intrastat_info[n]['weight']} kg |"
+                    f" {intrastat_info[n]['price_subtotal']} €"
+                    for n in intrastat_info
+                ]
+            )
 
-    def action_remove_intrastat_data_in_move_lines(self):
+    def action_remove_intrastat_data_invoice(self):
         for move in self.filtered(lambda x: x.move_type.startswith("out_")):
             for line in move.invoice_line_ids.filtered(
                 lambda x: x.product_id
@@ -59,3 +94,8 @@ class AccountMove(models.Model):
                 and "HS CODE" in x.name
             ):
                 line.name = re.compile("\nHS CODE.*").sub("", line.name)
+            if "HS Codes:" in move.narration:
+                move.narration = re.compile("\nHS Codes:.*").sub("", move.narration)
+                move.narration = re.compile("\n[0-9]{4,12}: .*€").sub(
+                    "", move.narration
+                )
