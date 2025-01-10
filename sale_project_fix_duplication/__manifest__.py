d376456a95e094cba890abf7858b81677d8bd044
sergiocorato <sergiocorato@gmail.com>
2025-01-10 16:03:08 +0100

    [FIX] pre-commit

--- sale_project_fix_duplication/__manifest__.py
+++ sale_project_fix_duplication/__manifest__.py
@@ -3,8 +3,8 @@
 {
     "name": "Sale project fix duplication DEPRECATED",
     "summary": "Simple module to integrate PR https://github.com/OCA/OCB/pull/1186 "
-               "Update: this module create a bug in manual project creation, not "
-               "simply resolvable.",
+    "Update: this module create a bug in manual project creation, not "
+    "simply resolvable.",
     "version": "14.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/efatto/e-account",
