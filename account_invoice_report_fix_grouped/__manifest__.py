666ed148dd9ac037a7688cc7859f82c1f85b6e25
sergiocorato <sergiocorato@gmail.com>
2025-01-22 14:49:50 +0100

    [IMP] new module to fix account invoice report on old db with group lines enabled

---
+++ account_invoice_report_fix_grouped/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Invoice report fix for db previously with grouped invoice lines",
+    "version": "14.0.1.0.0",
+    "category": "Invoice Management",
+    "license": "AGPL-3",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-account",
+    "depends": [
+        "account",
+    ],
+    "data": [],
+    "installable": True,
+}
