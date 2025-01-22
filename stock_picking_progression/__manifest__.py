bd63958525ac4f89ad3bf6569f2680bd3d912182
sergiocorato <sergiocorato@gmail.com>
2025-01-22 09:26:05 +0100

    [ADD] stock picking progression module

---
+++ stock_picking_progression/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock picking progression",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "summary": "Add progression on picking",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "website": "https://github.com/efatto/e-account",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_picking_progression/models/stock_picking.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    progress = fields.Float(compute="_compute_qty", store=True, string=" ")
+    progress_text = fields.Char(
+        string="Progress",
+        store=True,
+        compute="_compute_qty",
+    )
+
+    @api.depends("move_lines", "move_lines.quantity_done", "move_lines.product_uom_qty")
+    def _compute_qty(self):
+        for record in self:
+            total_qty = sum(record.mapped("move_lines.product_uom_qty"))
+            done_qty = sum(record.mapped("move_lines.quantity_done"))
+            record.progress = (
+                100 if record.state == "done" else (done_qty / (total_qty or 1))
+            )
+            record.progress_text = "%s/%s" % (
+                done_qty,
+                total_qty,
+            )
