92feedc0e6542866744eb76703b2f92e9b8d0905
sergiocorato <sergiocorato@gmail.com>
2025-03-03 16:21:57 +0100

    [FIX] remove add of archived journals as they are ignored - add constrains to archive only not used journals

--- account_financial_report_filter_journal/models/account_journal.py
+++ account_financial_report_filter_journal/models/account_journal.py
@@ -1,9 +1,20 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo.exceptions import ValidationError
+
+from odoo import fields, models, api, _
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     trial_balance_exclude = fields.Boolean()
+
+    @api.constrains("active")
+    def _check_journal_moves(self):
+        for journal in self:
+            if not journal.active and self.env["account.move"].search([
+                ("journal_id", "=", journal.id),
+            ]):
+                raise ValidationError(
+                    _("This journal contains moves and cannot be archived!"))
