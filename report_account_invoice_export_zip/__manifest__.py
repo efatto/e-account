821a5067ccf5c339d7b871bd6bce0974abc76cfe
sergiocorato <sergiocorato@gmail.com>
2025-03-07 15:07:02 +0100

    [FIX] report_account_invoice_export_zip: black, isort, prettier

--- report_account_invoice_export_zip/__manifest__.py
+++ report_account_invoice_export_zip/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2018 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Export e-invoice report in zip file',
-    'version': '14.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'Add ability to export more report of e-invoice in a zip file.',
-    'website': 'http://www.efatto.it',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_it_fatturapa_in',
+    "name": "Export e-invoice report in ZIP file",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "description": "Export several report of e-invoice in a ZIP file.",
+    "website": "https://github.com/efatto/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_in",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizards/wizard_export.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/wizard_export.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- report_account_invoice_export_zip/wizards/wizard_export.py
+++ report_account_invoice_export_zip/wizards/wizard_export.py
@@ -1,10 +1,10 @@
+import base64
+import os
+import tempfile
+import zipfile
 
 from odoo import fields, models
 from odoo.tools import config
-import base64
-import zipfile
-import os
-import tempfile
 
 
 class WizardAccountInvoiceExport(models.TransientModel):
@@ -12,54 +12,60 @@ class WizardAccountInvoiceExport(models.TransientModel):
 
     export_report_name = fields.Char()
     data = fields.Binary("File", readonly=True)
-    name = fields.Char('Filename', size=32, readonly=True)
+    name = fields.Char("Filename", size=32, readonly=True)
 
     def export_report(self):
-        attachment_obj = self.env['ir.attachment']
+        attachment_obj = self.env["ir.attachment"]
         attachments = attachment_obj.browse()
         for attach in self.env["fatturapa.attachment.in"].browse(
-                self._context['active_ids']
+            self._context["active_ids"]
         ):
             fatturapa_attachment_model = self.env["fatturapa.attachment"]
             html = fatturapa_attachment_model.get_fattura_elettronica_preview(attach)
             pdf = self.env["ir.actions.report"]._run_wkhtmltopdf([html])
             result = base64.b64encode(pdf)
-            file_name = '%s_%s.pdf' % (
+            file_name = "%s_%s.pdf" % (
                 attach.xml_supplier_id.name,
-                attach.invoices_number
+                attach.invoices_number,
             )
-            att = attachment_obj.create({
-                'name': file_name,
-                'datas': result,
-                'res_model': attach._name,
-                'res_id': attach.id,
-                'type': 'binary'
-            })
+            att = attachment_obj.create(
+                {
+                    "name": file_name,
+                    "datas": result,
+                    "res_model": attach._name,
+                    "res_id": attach.id,
+                    "type": "binary",
+                }
+            )
             attachments |= att
 
-        path = os.path.join(config['data_dir'], "filestore", self.env.cr.dbname)
+        path = os.path.join(config["data_dir"], "filestore", self.env.cr.dbname)
         compression = zipfile.ZIP_STORED
-        temp = tempfile.mktemp(suffix='.zip')
+        temp = tempfile.mktemp(suffix=".zip")
         zf = zipfile.ZipFile(temp, mode="w")
         for attachment in attachments:
             file_name = attachment.store_fname
-            zf.write(os.path.join(path, file_name),
-                     attachment.name.replace('/', '_'),
-                     compress_type=compression)
+            zf.write(
+                os.path.join(path, file_name),
+                attachment.name.replace("/", "_"),
+                compress_type=compression,
+            )
         zf.close()
-        data = open(temp, 'rb').read()
-        export_report_name = self.export_report_name or 'Zip export report'
-        zip_att = attachment_obj.create({
-            'name': export_report_name + '.zip',
-            'datas': base64.encodebytes(data),
-            'type': 'binary'
-        })
+        data = open(temp, "rb").read()
+        export_report_name = self.export_report_name or "Zip export report"
+        zip_att = attachment_obj.create(
+            {
+                "name": export_report_name + ".zip",
+                "datas": base64.encodebytes(data),
+                "type": "binary",
+            }
+        )
         return {
-            'view_type': 'form',
-            'name': "Export Invoices",
-            'res_id': zip_att.id,
-            'view_mode': 'form',
-            'res_model': 'ir.attachment',
-            'type': 'ir.actions.act_window',
-            'context': self._context,
+            "view_type": "form",
+            "name": "Export Invoices",
+            "res_id": zip_att.id,
+            "view_mode": "form",
+            "res_model": "ir.attachment",
+            "type": "ir.actions.act_window",
+            "context": self._context,
         }
