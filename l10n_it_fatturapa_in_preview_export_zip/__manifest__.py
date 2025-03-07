c7708767764b2ca9ce6b566cdeddb807633d175b
sergiocorato <sergiocorato@gmail.com>
2025-03-07 15:10:23 +0100

    [FIX] pre-commit

--- l10n_it_fatturapa_in_preview_export_zip/__manifest__.py
+++ l10n_it_fatturapa_in_preview_export_zip/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2018 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Export e-invoice report in ZIP file",
+    "name": "Export e-invoice preview in ZIP file",
     "version": "14.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "description": "Export several report of e-invoice in a ZIP file.",
+    "summary": "Export several preview of e-invoice in a ZIP file.",
     "website": "https://github.com/efatto/e-account",
     "license": "AGPL-3",
     "depends": [
