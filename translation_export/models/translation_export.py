1b9d483f551a9d3cbaff184ae900ac8fe2758efc
sergiocorato <sergiocorato@gmail.com>
2025-03-19 19:24:25 +0100

    [FIX] translate

--- translation_export/models/translation_export.py
+++ translation_export/models/translation_export.py
@@ -24,17 +24,20 @@ class TranslationExport(models.Model):
         report = self.env.ref("translation_export.xlsx_translation_export")
         attachment_obj = self.env["ir.attachment"]
         for rec in self:
-            existing_attachments = attachment_obj.search([
-                ("res_model", "=", rec._name),
-                ("res_id", "=", rec.id),
-            ], order="id desc")
-            if len(existing_attachments) > rec.max_export_number:
-                existing_attachments[:-rec.max_export_number].unlink()
+            existing_attachments = attachment_obj.search(
+                [
+                    ("res_model", "=", rec._name),
+                    ("res_id", "=", rec.id),
+                ],
+                order="id desc",
+            )
+            if len(existing_attachments) >= rec.max_export_number:
+                existing_attachments[rec.max_export_number - 1 :].unlink()
             report_content, report_type = report._render_xlsx(rec.ids, [])
             result = base64.b64encode(report_content)
             file_name = "%s_%s.xlsx" % (
                 rec.name,
-                fields.Date.context_today(rec).strftime("%d_%m_%Y"),
+                fields.Datetime.now().strftime("%d_%m_%Y_%HH_%MM_%SS"),
             )
             attachment_obj.create(
                 {
