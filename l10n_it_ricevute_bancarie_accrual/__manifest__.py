773d73244293d2b0d4ba23f6707f1f689551a897
sergiocorato <sergiocorato@gmail.com>
2025-03-28 18:45:32 +0100

    [FIX] pre-commit

--- l10n_it_ricevute_bancarie_accrual/__manifest__.py
+++ l10n_it_ricevute_bancarie_accrual/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Accounting',
     'summary': 'Ricevute bancarie con maturazione',
     'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-account',
+    'website': 'https://github.com/efatto/e-account',
     'license': 'AGPL-3',
     'depends': [
         'l10n_it_ricevute_bancarie',
