cd815d6ff88f045f92e3b9f8963d1bc009ef4894
sergiocorato <sergiocorato@gmail.com>
2025-03-28 18:45:32 +0100

    [FIX] pre-commit

--- product_pricelist_rounding/__manifest__.py
+++ product_pricelist_rounding/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Products',
     'author': 'Sergio Corato',
     'description': 'Add option to select rouding method on pricelist.',
-    'website': 'https://github.com/sergiocorato/e-account',
+    'website': 'https://github.com/efatto/e-account',
     'license': 'AGPL-3',
     'depends': [
         'product',
