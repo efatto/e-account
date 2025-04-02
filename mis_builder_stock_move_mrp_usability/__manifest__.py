fdfd7002f0a5b4048653e61a0b228abb9a96b7a0
sergiocorato <sergiocorato@gmail.com>
2025-04-02 08:22:52 +0200

    [ADD] new module to show price from bom in MIS

---
+++ mis_builder_stock_move_mrp_usability/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Glue module from stock move + MRP + MIS',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'author': 'Sergio Corato',
+    'summary': 'Add fields from MRP Bom evalution in stock move and show in MIS Builder'
+               ' report.',
+    'website': 'https://github.com/efatto/e-account',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+        "mis_builder_query_drilldown_view",
+        "mrp_bom_evaluation",
+    ],
+    'data': [
+        "views/stock.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+}
---
+++ mis_builder_stock_move_mrp_usability/models/stock.py
@@ -0,0 +1,17 @@
+
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    bom_price_total = fields.Float(
+        string="Price Total from BOM", compute='_compute_bom_price_total', store=True)
+
+    @api.multi
+    @api.depends('bom_line_price_unit', 'price_unit', 'quantity_done')
+    def _compute_bom_price_total(self):
+        for move in self:
+            move.bom_price_total = move.quantity_done * (
+                move.bom_line_price_unit or move.price_unit
+            )
