1b3d06b6495003c7fd57679a7b5c93e24bb492cc
sergiocorato <sergiocorato@gmail.com>
2025-06-11 11:00:15 +0200

    [IMP] add more total costs in sale order line for mis builder

--- sale_order_line_usability/models/sale.py
+++ sale_order_line_usability/models/sale.py
@@ -12,6 +12,10 @@ class SaleOrderLine(models.Model):
     move_price_unit = fields.Float(
         string="Move Cost Unit", compute='_get_move_price_unit',
         inverse='_set_move_price_unit', store=True)
+    move_price_delivering_total = fields.Float(
+        string="Total Delivering Costs", compute='_get_move_price_unit', store=False)
+    move_price_invoiced_total = fields.Float(
+        string="Total Invoiced Costs", compute='_get_move_price_unit', store=False)
     move_price_to_invoice_total = fields.Float(
         string="Total Cost To Invoice", compute='_get_move_price_unit', store=False,
         help="This cost is only computed when products can be sold but not purchased.")
@@ -23,15 +27,20 @@ class SaleOrderLine(models.Model):
             # preserve negative values as costs
             line.move_price_unit = min(
                 [-abs(x.price_unit) for x in line.move_ids] or [0])
+            price = (
+               line.move_price_unit if line.move_price_unit != 0.0 else
+               - line.product_standard_price
+            )
+            qty_max_delivery = max([line.qty_delivered, line.product_qty])
+            line.move_price_delivering_total =  price * qty_max_delivery
+            line.move_price_invoiced_total = price * line.qty_invoiced
             if line.product_id.purchase_ok:
                 # this product can be purchased so if we consider it could lead to a
                 # duplication of costs
                 line.move_price_to_invoice_total = 0
             else:
-                line.move_price_to_invoice_total = (
-                    line.move_price_unit if line.move_price_unit != 0.0 else
-                    - line.product_standard_price) * (
-                    max([line.qty_delivered, line.product_qty]) - line.qty_invoiced
+                line.move_price_to_invoice_total = price * (
+                    qty_max_delivery - line.qty_invoiced
                 )
 
     @api.one
