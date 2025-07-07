62fd6e0b630420ccf39537268a3c5ef5e63dcdb9
sergiocorato <sergiocorato@gmail.com>
2025-07-07 08:43:47 +0200

    [IMP] account_e_invoice_date_editable: black, isort, prettier

--- account_e_invoice_date_editable/__manifest__.py
+++ account_e_invoice_date_editable/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'E-invoice received date editable',
-    'version': '12.0.1.0.0',
-    'category': 'Invoice Management',
-    'license': 'AGPL-3',
-    'summary': "Add the ability to always set e-invoice received date",
-    'author': "Sergio Corato",
-    'website': 'https://github.com/sergiocorato/e-account',
-    'depends': [
-        'l10n_it_fatturapa_in',
+    "name": "E-invoice received date editable",
+    "version": "14.0.1.0.0",
+    "category": "Invoice Management",
+    "license": "AGPL-3",
+    "summary": "Add the ability to always set e-invoice received date",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-account",
+    "depends": [
+        "l10n_it_fatturapa_in",
     ],
-    'data': [
-        'views/invoice_view.xml',
+    "data": [
+        "views/invoice_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
