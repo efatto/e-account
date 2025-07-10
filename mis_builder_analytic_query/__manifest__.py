795483e84ee32cb719fc24a42d6cb0079729ebb1
sergiocorato <sergiocorato@gmail.com>
2025-07-10 18:47:49 +0200

    [IMP] mis_builder_analytic_query: black, isort, prettier

--- mis_builder_analytic_query/__manifest__.py
+++ mis_builder_analytic_query/__manifest__.py
@@ -1,10 +1,8 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-
 {
     "name": "Mis builder query analytic",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Reporting",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
