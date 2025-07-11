185efa15ec70532d8eaf7adac0b819bef0019bb9
sergiocorato <sergiocorato@gmail.com>
2025-07-11 11:28:13 +0200

    [IMP] stock_move_usability: black, isort, prettier

--- stock_move_usability/__manifest__.py
+++ stock_move_usability/__manifest__.py
@@ -1,17 +1,16 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Stock move usability',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'author': 'Sergio Corato',
-    'summary': 'Add some useful fields in stock move',
-    'website': 'https://github.com/efatto/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock',
+    "name": "Stock move usability",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Sergio Corato",
+    "summary": "Add a price total field in stock move",
+    "website": "https://github.com/efatto/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
