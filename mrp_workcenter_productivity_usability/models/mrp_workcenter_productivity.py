4a4e1622820bb4635e8fbd27ffbb9cf681782889
sergiocorato <sergiocorato@gmail.com>
2025-07-11 18:14:12 +0200

    [IMP] mrp_workcenter_productivity_usability: black, isort, prettier

--- mrp_workcenter_productivity_usability/models/mrp_workcenter_productivity.py
+++ mrp_workcenter_productivity_usability/models/mrp_workcenter_productivity.py
@@ -1,15 +1,14 @@
-
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class MrpWorkcenterProductivity(models.Model):
-    _inherit = 'mrp.workcenter.productivity'
+    _inherit = "mrp.workcenter.productivity"
 
     duration_hour = fields.Float(
-        string="Duration (hours)", compute='_compute_duration_hour', store=True)
+        string="Duration (hours)", compute="_compute_duration_hour", store=True
+    )
 
-    @api.multi
-    @api.depends('duration')
+    @api.depends("duration")
     def _compute_duration_hour(self):
         for prod in self:
             prod.duration_hour = prod.duration / 60.0
