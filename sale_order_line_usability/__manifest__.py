7669aa4ecf11d91dcfa7e0cf614a0cd519c172d3
sergiocorato <sergiocorato@gmail.com>
2025-07-11 18:29:49 +0200

    [IMP] sale_order_line_usability: black, isort, prettier

--- sale_order_line_usability/__manifest__.py
+++ sale_order_line_usability/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale order line usability',
-    'version': '12.0.1.0.1',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'Add order line product standard price link.',
-    'website': 'https://github.com/efatto/e-account',
-    'license': 'AGPL-3',
-    'depends': [
-        'delivery',
-        'sale_stock',
+    "name": "Sale order line usability",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Add order line product standard price link.",
+    "website": "https://github.com/efatto/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "delivery",
+        "sale_stock",
     ],
-    'data': [
-        'views/sale_order_line_views.xml',
+    "data": [
+        "views/sale_order_line_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
