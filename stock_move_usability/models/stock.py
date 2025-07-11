b546390dc8bd746dcaa6705614ab90529fda7e93
sergiocorato <sergiocorato@gmail.com>
2025-07-11 11:28:13 +0200

    [IMP] stock_move_usability: black, isort, prettier

--- stock_move_usability/models/stock.py
+++ stock_move_usability/models/stock.py
@@ -1,15 +1,14 @@
-
 from odoo import api, fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     price_total = fields.Float(
-        string="Price Total", compute='_compute_price_total', store=True)
+        string="Price Total", compute="_compute_price_total", store=True
+    )
 
-    @api.multi
-    @api.depends('price_unit', 'quantity_done')
+    @api.depends("price_unit", "quantity_done")
     def _compute_price_total(self):
         for move in self:
             move.price_total = move.quantity_done * move.price_unit
