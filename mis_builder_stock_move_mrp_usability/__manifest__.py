b306bb99d62df42f0a7e3401d38a3540e0adff33
sergiocorato <sergiocorato@gmail.com>
2025-08-01 14:09:52 +0200

    [IMP] remove auto-install from mis_builder_stock_move_mrp_usability

--- mis_builder_stock_move_mrp_usability/__manifest__.py
+++ mis_builder_stock_move_mrp_usability/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Glue module from stock move + MRP + MIS',
+    'name': 'Glue module from stock move + MRP + MIS - DEPRECATED',
     'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'author': 'Sergio Corato',
@@ -18,5 +18,5 @@
         "views/stock.xml",
     ],
     'installable': True,
-    'auto_install': True,
+    'auto_install': False,
 }
