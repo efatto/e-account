fede2a0f9c298f3946a845152417b511133eb7bb
sergiocorato <sergiocorato@gmail.com>
2025-09-25 09:15:11 +0200

    [IMP] help on field

--- res_partner_email_shipping/models/partner.py
+++ res_partner_email_shipping/models/partner.py
@@ -4,7 +4,10 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    email_shipping = fields.Char(string="Email Shipping")
+    email_shipping = fields.Char(
+        string="Email Shipping",
+        help="Email address that can be used in email shipping templates",
+    )
     email_shipping_template_id = fields.Many2one(
         comodel_name="mail.template",
         string="Shipping Email Template",
