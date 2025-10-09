fd4be2103fd84cfeb66cffe317323540ccfc02ae
sergiocorato <sergiocorato@gmail.com>
2025-10-09 09:46:07 +0200

    [FIX] removed wrong license

--- sale_order_date_order_visibility/__manifest__.py
+++ sale_order_date_order_visibility/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale order project, analytic and opportunity visibility",
     "version": "14.0.1.0.0",
