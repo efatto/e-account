9b3717668200f2ace958bdc6987a5274b15eaa34
sergiocorato <sergiocorato@gmail.com>
2025-10-09 09:55:15 +0200

    [IMP] add new module account_invoice_report_due_list_optional

---
+++ account_invoice_report_due_list_optional/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Invoice Due list Optional Visibility",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Add option to make due list always visible.",
+    "website": "https://github.com/efatto/e-account",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_report_due_list",
+    ],
+    "data": [
+        "views/res_config_settings_view.xml",
+    ],
+    "installable": True,
+}
