809100bde8c79717ab9fe3709491416d8508f26f
sergiocorato <sergiocorato@gmail.com>
2025-10-21 16:02:00 +0200

    [ADD] module to set order on create date on invoice only

---
+++ account_invoice_order_create_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account invoice order by create date",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-account",
+    "license": "AGPL-3",
+    "category": "other",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/invoice_view.xml",
+    ],
+    "summary": "Set order of account invoice on creation date",
+    "installable": True,
+}
