4247621ea37421345fee6141cad1080c0f13fc49
sergiocorato <sergiocorato@gmail.com>
2025-10-21 15:41:12 +0200

    [FIX] remove unused module l10n_it_fatturapa_in_received_dt

--- l10n_it_fatturapa_in_received_dt/models/account_move.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class AccountInvoice(models.Model):
-    _inherit = "account.move"
-
-    e_invoice_received_date = fields.Datetime(string="E-Bill Received Datetime")
