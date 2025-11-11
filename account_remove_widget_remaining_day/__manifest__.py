50fff9441cff731355ba9b0b4059198f02b87730
sergiocorato <sergiocorato@gmail.com>
2025-11-11 09:48:48 +0100

    [FIX] split module account_remove_widget_remaining_day for purchase part

--- account_remove_widget_remaining_day/__manifest__.py
+++ account_remove_widget_remaining_day/__manifest__.py
@@ -1,19 +1,17 @@
 # Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Remove widget remaining days in account and purchase",
-    "version": "14.0.1.0.0",
+    "name": "Remove widget remaining days in account view",
+    "version": "14.0.1.0.1",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
     "depends": [
         "account",
-        "purchase",
     ],
     "data": [
         "views/account_invoice_view.xml",
-        "views/purchase_view.xml",
     ],
     "installable": True,
 }
