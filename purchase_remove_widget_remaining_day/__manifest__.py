54288d5d3931f13f434de3048475f85ade6898a2
sergiocorato <sergiocorato@gmail.com>
2025-11-11 09:53:20 +0100

    [ADD] module purchase_remove_widget_remaining_day

---
+++ purchase_remove_widget_remaining_day/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Remove widget remaining days in purchase view",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-account",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase_view.xml",
+    ],
+    "installable": True,
+}
