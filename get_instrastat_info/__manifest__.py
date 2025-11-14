13c8e17a3e4d94b51f40e60ae7f4b536c0594b0f
sergiocorato <sergiocorato@gmail.com>
2025-11-14 16:45:03 +0100

    [ADD] new module get_instrastat_info

---
+++ get_instrastat_info/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mixin module to get Intrastat info",
+    "summary": "Get Intrastat info to put in reports",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/efatto/e-account",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_it_intrastat",
+    ],
+    "data": [],
+}
