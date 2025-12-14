aa0835e2a0902c2b5b21c8fed156203b3aefbfc9
sergiocorato <sergiocorato@gmail.com>
2025-12-14 17:51:59 +0100

    [FIX] pre-commit

---
+++ sale_timesheet_profitability_query_fix/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale timesheet profitability multiple project query fix",
+    "version": "14.0.1.0.0",
+    "category": "Timesheet",
+    "license": "AGPL-3",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-account",
+    "summary": "This module fix the query to support multiple project linked "
+    "to the same analytic account",
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [],
+    "installable": True,
+}
