cf06975cbc3f70d7f0576616567ca2e4dc2104f1
sergiocorato <sergiocorato@gmail.com>
2025-12-17 15:06:48 +0100

    [MIG] account_balance_line_progressive to 16.0

--- account_balance_line_progressive/models/account_move_line.py
+++ account_balance_line_progressive/models/account_move_line.py
@@ -1,6 +1,3 @@
-# Copyright 2019-2023 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from odoo import fields, models
 
 
@@ -9,11 +6,21 @@ class AccountMoveLine(models.Model):
     _order = "date desc, credit desc, id desc"
 
     def _compute_balance_progressive(self):
-        tables, where_clause, where_params = self.with_context(
-            initial_bal=True
-        )._query_get()
-        where_params = [tuple(self.ids)] + where_params
-        query = """SELECT l1.id AS line_id,
+        query = self._where_calc(
+            list(self.env.context.get("domain_cumulated_balance") or [])
+        )
+        # order_string = ", ".join(self._generate_order_by_inner(
+        #     self._table,
+        #     self.env.context.get(
+        #         'order_cumulated_balance'),
+        #     query,
+        #     reverse_direction=True))
+        from_clause, where_clause, where_clause_params = query.get_sql()
+        # tables, where_clause, where_params = self.with_context(
+        #     initial_bal=True
+        # )._query_get()
+        where_clause_params = [tuple(self.ids)] + where_clause_params
+        sql = """SELECT l1.id AS line_id,
             COALESCE(SUM(l2.debit-l2.credit), 0) AS balance,
             CASE WHEN l1.currency_id <> l1.company_currency_id
                     AND l2.currency_id <> l2.company_currency_id
@@ -25,15 +32,13 @@ class AccountMoveLine(models.Model):
             FROM account_move_line l1
             LEFT JOIN account_account a
             ON (a.id = l1.account_id)
-            LEFT JOIN account_account_type at
-            ON (at.id = a.user_type_id)
             JOIN account_move m on (m.id = l1.move_id AND m.state <> 'draft')
             LEFT JOIN account_move_line l2
             ON (l1.account_id = l2.account_id
                 AND (
                      l1.partner_id = l2.partner_id
                      OR
-                     at.type not in ('receivable', 'payable')
+                     a.account_type not in ('asset_receivable', 'liability_payable')
                     )
                )
             AND (
@@ -46,12 +51,12 @@ class AccountMoveLine(models.Model):
         if where_clause:
             where_clause = "AND " + where_clause
             where_clause = where_clause.replace("account_move_line", "l1")
-            query += where_clause
-        query += (
+            sql += where_clause
+        sql += (
             " GROUP BY l1.id, l1.currency_id, l1.company_currency_id,"
             " l2.currency_id, l2.company_currency_id"
         )
-        self._cr.execute(query, where_params)
+        self._cr.execute(sql, where_clause_params)
         result = self._cr.fetchall()
         if result:
             lines_not_in_result = [
@@ -70,13 +75,11 @@ class AccountMoveLine(models.Model):
     balance_progressive = fields.Monetary(
         compute="_compute_balance_progressive",
         currency_field="company_currency_id",
-        string="Balance Progressive",
         help="Field holding the progressive balance of the account for partner",
     )
     balance_progressive_currency = fields.Monetary(
         compute="_compute_balance_progressive",
         currency_field="currency_id",
-        string="Currency Balance Progressive",
         help="Field holding the progressive currency balance of the account for "
         "partner",
     )
