9719f3b12440b6764e989b16f8d1e901d01b4c35
sergiocorato <sergiocorato@gmail.com>
2025-12-17 15:06:48 +0100

    [MIG] account_balance_line_progressive to 16.0

--- account_balance_line_progressive/__manifest__.py
+++ account_balance_line_progressive/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2016-2023 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2016 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account balance line progressive",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
