52e73ba15a788846cc4be783b7fcdfce208f7f66
sergiocorato <sergiocorato@gmail.com>
2025-12-24 12:07:29 +0100

    [FIX] remove progress from grouped columns

--- stock_picking_progression/models/stock_picking.py
+++ stock_picking_progression/models/stock_picking.py
@@ -4,18 +4,23 @@ from odoo import api, fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    progress = fields.Float(compute="_compute_qty", store=True, string=" ")
+    progress = fields.Float(
+        compute="_compute_qty", store=True, string=" ", group_operator=None)
     progress_text = fields.Char(
         string="Progress",
         store=True,
         compute="_compute_qty",
     )
+    total_qty = fields.Float(compute="_compute_qty", store=True, group_operator="sum")
+    done_qty = fields.Float(compute="_compute_qty", store=True, group_operator="sum")
 
     @api.depends("move_lines", "move_lines.quantity_done", "move_lines.product_uom_qty")
     def _compute_qty(self):
         for record in self:
             total_qty = sum(record.mapped("move_lines.product_uom_qty"))
+            record.total_qty = total_qty
             done_qty = sum(record.mapped("move_lines.quantity_done"))
+            record.done_qty = done_qty
             record.progress = (
                 100
                 if (record.state == "done" or not total_qty)
