a1fca466deab6ea2ea68bb49cd91b2036b1e2f3c
sergiocorato <sergiocorato@gmail.com>
2025-12-29 15:17:42 +0100

    [MIG] account_sequence_accountant to 16.0

--- account_sequence_accountant/__manifest__.py
+++ account_sequence_accountant/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2017-2022 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2017 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sequence access to accountant",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
