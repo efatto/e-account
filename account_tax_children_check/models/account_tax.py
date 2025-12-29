6bbfc3e71a0f5f682b9f256349ae1e559baf2382
sergiocorato <sergiocorato@gmail.com>
2025-12-29 15:23:13 +0100

    [MIG] account_tax_children_check to 16.0

--- account_tax_children_check/models/account_tax.py
+++ account_tax_children_check/models/account_tax.py
@@ -11,15 +11,15 @@ class AccountTax(models.Model):
             for child_tax in tax.children_tax_ids:
                 if len(child_tax.parent_tax_ids) > 1:
                     raise UserError(
-                        _("Tax %s has already a parent tax: %s")
-                        % (
-                            child_tax.name,
-                            ", ".join(
+                        _(
+                            "Tax %(name)s has already a parent tax: %(parent)s",
+                            name=child_tax.name,
+                            parent=", ".join(
                                 [
                                     "[%s] %s" % (i, name)
                                     for i, name in enumerate(
                                         child_tax.parent_tax_ids.filtered(
-                                            lambda x: x != tax
+                                            lambda x, t=tax: x != t
                                         ).mapped("name"),
                                         1,
                                     )
