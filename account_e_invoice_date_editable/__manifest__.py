89442dd95502766be8ddda8c91b90eb8a3562b44
sergiocorato <sergiocorato@gmail.com>
2025-12-29 10:04:25 +0100

    [MIG] account_e_invoice_date_editable to 16.0

--- account_e_invoice_date_editable/__manifest__.py
+++ account_e_invoice_date_editable/__manifest__.py
@@ -2,10 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "E-invoice received date editable",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Invoice Management",
     "license": "AGPL-3",
-    "summary": "Add the ability to always set e-invoice received date",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
     "depends": [
---
+++ account_e_invoice_date_editable/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module add the ability to always set the e-invoice received date in vendor invoices/credit notes.
