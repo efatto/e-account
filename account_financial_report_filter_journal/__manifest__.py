18c5759a6d7d79103d47bee12de14f6b94273004
sergiocorato <sergiocorato@gmail.com>
2025-12-29 10:49:50 +0100

    [MIG] account_financial_report_filter_journal

--- account_financial_report_filter_journal/__manifest__.py
+++ account_financial_report_filter_journal/__manifest__.py
@@ -1,9 +1,8 @@
-# Copyright 2017-2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2017 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "Account financial report default filter journal",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
