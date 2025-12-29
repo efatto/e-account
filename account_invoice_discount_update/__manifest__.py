60fd2a00f5d791e80ae60ed07a19de1bf5474db9
sergiocorato <sergiocorato@gmail.com>
2025-12-29 11:42:42 +0100

    [MIG] account_invoice_discount_update to 16.0

--- account_invoice_discount_update/__manifest__.py
+++ account_invoice_discount_update/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2020-2023 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Update discount in invoice",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Invoice Management",
     "license": "AGPL-3",
     "author": "Sergio Corato",
