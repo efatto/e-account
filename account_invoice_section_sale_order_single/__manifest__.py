4cfb857d553d3d372b87720e69db8277c74f0fbc
sergiocorato <sergiocorato@gmail.com>
2025-12-29 14:34:29 +0100

    [MIG] account_invoice_section_sale_order_single to 16.0

--- account_invoice_section_sale_order_single/__manifest__.py
+++ account_invoice_section_sale_order_single/__manifest__.py
@@ -2,10 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account invoice force group for one single sale order",
-    "version": "14.0.1.0.2",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "This module force adding the group with one single sale order too",
     "website": "https://github.com/efatto/e-account",
     "license": "AGPL-3",
     "depends": [
