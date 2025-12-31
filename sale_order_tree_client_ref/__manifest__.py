7954ae018f32a266426168ce9eecd369d8651146
sergiocorato <sergiocorato@gmail.com>
2025-12-31 16:20:23 +0100

    [MIG] sale_order_tree_client_ref to 16.0

--- sale_order_tree_client_ref/__manifest__.py
+++ sale_order_tree_client_ref/__manifest__.py
@@ -2,11 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale order tree and product",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Add sale order ref in tree view and sale description in product tree "
-    "view.",
     "website": "https://github.com/efatto/e-account",
     "license": "AGPL-3",
     "depends": [
