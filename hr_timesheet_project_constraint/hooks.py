d94e37776d85840c3ae824be28dbcf2bf0493ca1
sergiocorato <sergiocorato@gmail.com>
2026-01-23 15:02:19 +0100

    [FIX] hr_timesheet_project_constraint hook (not yet active)

--- hr_timesheet_project_constraint/hooks.py
+++ hr_timesheet_project_constraint/hooks.py
@@ -7,9 +7,11 @@ def pre_init_hook(cr):
     _logger.info("Ensure project has the same analytic account of the analytic line")
     cr.execute(
         """
-        UPDATE account_analytic_line aal SET account_id = p.analytic_account_id
-        LEFT JOIN project_project p ON p.id = aal.project_id
-        WHERE aal.project_id IS NOT NULL
+        UPDATE account_analytic_line aal
+        SET account_id = p.analytic_account_id
+        FROM project_project p
+        WHERE p.id = aal.project_id
+        AND aal.project_id IS NOT NULL
         AND p.analytic_account_id IS NOT NULL
         AND aal.account_id != p.analytic_account_id
         """
