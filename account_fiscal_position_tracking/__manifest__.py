2716bfa96b9cc7c57a2e6b7d14a16d9c6f25b390
sergiocorato <sergiocorato@gmail.com>
2026-02-23 11:44:33 +0100

    [IMP] translation account_fiscal_position_tracking

--- account_fiscal_position_tracking/__manifest__.py
+++ account_fiscal_position_tracking/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Add chatter to account fiscal position",
+    "name": "Add chatter and constraints to account fiscal position",
     "version": "14.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
