db651891ccc96a1f0db4b1f2dd1b23bbf6446dc9
sergiocorato <sergiocorato@gmail.com>
2026-02-23 10:46:05 +0100

    [IMP] add constraint on tax mapping in fiscal position

--- account_fiscal_position_tracking/models/account_fiscal_position.py
+++ account_fiscal_position_tracking/models/account_fiscal_position.py
@@ -1,6 +1,19 @@
-from odoo import models
+from odoo import models, api, _
+from odoo.exceptions import ValidationError
 
 
 class AccountFiscalPosition(models.Model):
     _name = "account.fiscal.position"
     _inherit = ["account.fiscal.position", "mail.thread"]
+
+
+class AccountFiscalPositionTax(models.Model):
+    _inherit = 'account.fiscal.position.tax'
+
+    @api.constrains('tax_src_id', 'tax_dest_id')
+    def _check_tax_mapping(self):
+        for tax_mapping in self:
+            if tax_mapping.tax_src_id == tax_mapping.tax_dest_id:
+                raise ValidationError(
+                    _('Taxes source and destination must be different.')
+                )
