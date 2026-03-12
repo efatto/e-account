4c96af626d1603f364918977d5dc3c125f1c0e41
sergiocorato <sergiocorato@gmail.com>
2026-03-12 10:14:41 +0100

    [ADD] module to group invoice lines by DDT and SO

---
+++ account_invoice_section_l10n_it_dn/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Acccount Invoice Section DDT and Sale Order",
+    "version": "14.0.1.0.0",
+    "summary": "Extension of Acccount Invoice Section Sale Order to allow "
+    "grouping of invoice lines according to DDT and Sale Order.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-account",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "depends": [
+        "account_invoice_section_sale_order",
+        "l10n_it_delivery_note",
+        "stock",
+    ],
+}
