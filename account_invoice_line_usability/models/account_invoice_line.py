3cbe771bfd81fad49c21d83f2d7f90b7de8a50e1
sergiocorato <sergiocorato@gmail.com>
2026-03-18 15:16:10 +0100

    [IMP] assign cost to account move line without linked stock moves

--- account_invoice_line_usability/models/account_invoice_line.py
+++ account_invoice_line_usability/models/account_invoice_line.py
@@ -10,20 +10,17 @@ class AccountInvoiceLine(models.Model):
     account_code = fields.Char(related="account_id.code")
     product_standard_price = fields.Float(related="product_id.standard_price")
     move_price_unit = fields.Float(
-        string="Move Price Unit", compute="_compute_move_price_unit", store=True
+        string="Unit Cost", compute="_compute_move_price_unit", store=True
     )
     move_price_total = fields.Float(
-        string="Price Total", compute="_compute_move_price_unit", store=True
+        string="Total Cost", compute="_compute_move_price_unit", store=True
     )
 
-    @api.depends("move_line_ids.price_unit")
+    @api.depends("move_line_ids.price_unit", "product_id", "product_id.standard_price")
     def _compute_move_price_unit(self):
         for line in self:
-            line.move_price_unit = min(
-                [-abs(x.price_unit) for x in line.move_line_ids] or [0]
-            )
-            line.move_price_total = line.quantity * (
-                line.move_price_unit
-                if line.move_price_unit != 0.0
-                else -line.product_standard_price
-            )
+            price_unit = min([-abs(x.price_unit) for x in line.move_line_ids] or [0])
+            if not price_unit:
+                price_unit = -line.product_id.standard_price
+            line.move_price_unit = price_unit
+            line.move_price_total = line.quantity * line.move_price_unit
