e5dbfd408080a061a14a106236f733411cc757e9
sergiocorato <sergiocorato@gmail.com>
2026-04-27 15:27:07 +0200

    [MIG] sale_order_confirmation_date to 18.0

--- sale_order_confirmation_date/__manifest__.py
+++ sale_order_confirmation_date/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale order confirmation date",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales Management",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
