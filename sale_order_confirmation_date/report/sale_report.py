f39bef25bc3e0276f71f460ce77a8de3c69d6dd8
sergiocorato <sergiocorato@gmail.com>
2026-04-27 15:27:07 +0200

    [MIG] sale_order_confirmation_date to 18.0

--- sale_order_confirmation_date/report/sale_report.py
+++ sale_order_confirmation_date/report/sale_report.py
@@ -6,6 +6,7 @@ class SaleReport(models.Model):
 
     confirmation_date = fields.Datetime(string="Confirmation date", readonly=True)
 
-    def _select_additional_fields(self, add_fields):
-        add_fields["confirmation_date"] = ", s.confirmation_date AS confirmation_date"
-        return super()._select_additional_fields(add_fields)
+    def _select_additional_fields(self):
+        res = super()._select_additional_fields()
+        res["confirmation_date"] = "s.confirmation_date"
+        return res
