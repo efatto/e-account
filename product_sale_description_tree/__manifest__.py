6efa0cf8e7c98ece5e60325a8e85164d464bae67
sergiocorato <sergiocorato@gmail.com>
2026-04-28 08:59:19 +0200

    [MIG] sale_order_tree_client_ref: Migration to 18.0

--- product_sale_description_tree/__manifest__.py
+++ product_sale_description_tree/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale order tree and product",
-    "version": "16.0.1.0.0",
+    "name": "Description Sale on product list view",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
@@ -12,7 +12,6 @@
     ],
     "data": [
         "views/product_view.xml",
-        "views/sale_order_view.xml",
     ],
     "installable": True,
 }
---
+++ product_sale_description_tree/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module add sale description in product tree view.
