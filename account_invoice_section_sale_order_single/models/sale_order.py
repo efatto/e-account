c231858b4d37e91033ff9a181cb6ae5c1185b211
sergiocorato <sergiocorato@gmail.com>
2026-05-05 11:07:44 +0200

    [MIG] account_invoice_section_sale_order_single: Migration to 18.0

--- account_invoice_section_sale_order_single/models/sale_order.py
+++ account_invoice_section_sale_order_single/models/sale_order.py
@@ -7,16 +7,16 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _create_invoices(self, grouped=False, final=False, date=None):
-        # do not put variable names as it conflicts with module
-        # sale_timesheet_invoice_description that rewrite the signature of the method
-        invoice_ids = super()._create_invoices(grouped, final, date)
-        for invoice in invoice_ids:
+        invoices = super()._create_invoices(grouped=grouped, final=final, date=date)
+        for invoice in invoices.sudo():
             if invoice.line_ids and (
                 len(invoice.line_ids.mapped(invoice.line_ids._get_section_grouping()))
                 == 1
             ):
                 # add section line for single group
                 sequence = 10
+                # Because invoices are already created, this would require
+                # an extra read access in order to read order fields.
                 move_lines = invoice._get_ordered_invoice_lines()
                 # Group move lines according to their sale order
                 section_grouping_matrix = OrderedDict()
@@ -48,10 +48,14 @@ class SaleOrder(models.Model):
                             )
                         )
                         sequence += 10
-                    for move_line in self.env["account.move.line"].browse(
-                        move_line_ids
+                    for move_line in (
+                        self.env["account.move.line"].sudo().browse(move_line_ids)
                     ):
+                        # Because invoices are already created, this would require
+                        # an extra write access in order to read order fields.
                         move_line.sequence = sequence
                         sequence += 10
+                # Because invoices are already created, this would require
+                # an extra write access in order to read order fields.
                 invoice.line_ids = section_lines
-        return invoice_ids
+        return invoices
--- account_invoice_section_sale_order_single/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-This module change logic of Account invoice section sale order module to group even when there is a single sale order invoiced.
