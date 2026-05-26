ed730edb083a6b674b66f147174eab13ca596c10
sergiocorato <sergiocorato@gmail.com>
2026-05-26 12:25:53 +0200

    [IMP] bump version l10n_it_account_xmlid_fix

--- l10n_it_account_xmlid_fix/__manifest__.py
+++ l10n_it_account_xmlid_fix/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Italy - Account XMLID Fix",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Localization/Italy",
     "summary": "Fix account XMLIDs before l10n_it installation",
     "author": "Sergio Corato",
