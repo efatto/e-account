ddfeb83380c00b65e1d318d10cf5f3d1065dca67
sergiocorato <sergiocorato@gmail.com>
2026-05-29 14:52:43 +0200

    [MIG] mail_no_follower_notification: Migration to 18.0

--- mail_no_follower_notification/__manifest__.py
+++ mail_no_follower_notification/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Show recipients of email",
     "summary": "Show the recipient of the email, removing the anonimous flag "
     "'Notify followers'.",
-    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)," "Sergio Corato",
+    "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
     "category": "Social Network",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "mail_optional_follower_notification",
--- mail_no_follower_notification/readme/CONTRIBUTORS.rst
+++
@@ -1,5 +0,0 @@
-* Adrien Peiffer <adrien.peiffer@acsone.eu>
-* Laurent Mignon <laurent.mignon@acsone.eu>
-* Andrea Stirpe <a.stirpe@onestein.nl>
-* Souheil Bejaoui <souheil.bejaoui@acsone.eu>
-* Sergio Corato <https://github.com/sergiocorato>
