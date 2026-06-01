771d0d007bdfe9c02fd2787b8d7dcdb3fae624fb
sergiocorato <sergiocorato@gmail.com>
2026-06-01 21:54:49 +0200

    [FIX] sale_order_comment_deprecated put fields as readonly and put in name that are deprecated

--- sale_order_comment_deprecated/models/sale_order.py
+++ sale_order_comment_deprecated/models/sale_order.py
@@ -7,11 +7,12 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    note1 = fields.Html("Top Comment")
-    note2 = fields.Html("Bottom Comment")
+    note1 = fields.Html("Top Comment (readonly as deprecated)", readonly=True)
+    note2 = fields.Html("Bottom Comment (readonly as deprecated)", readonly=True)
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    formatted_note = fields.Html("Formatted Note")
+    formatted_note = fields.Html(
+        "Formatted Note (readonly as deprecated)", readonly=True)
