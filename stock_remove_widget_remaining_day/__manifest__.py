0877a64768ffa20e182d837b0ed46050ccbf7416
sergiocorato <sergiocorato@gmail.com>
2026-06-26 07:22:11 +0200

    [MIG] stock_remove_widget_remaining_day: Migration to 18.0

--- stock_remove_widget_remaining_day/__manifest__.py
+++ stock_remove_widget_remaining_day/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Remove widget remaining days in picking and MRP",
-    "version": "14.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Stock Management",
     "license": "AGPL-3",
     "author": "Sergio Corato",
