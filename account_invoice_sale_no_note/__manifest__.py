2fd6701679353faebcaa85c86e6e42ceba7527a5
Sergio Corato <sergiocorato@gmail.com>
2026-09-21 12:18:31 +0200

    [MIG] account_invoice_sale_no_note: Migration to 18.0

--- account_invoice_sale_no_note/__manifest__.py
+++ account_invoice_sale_no_note/__manifest__.py
@@ -1,9 +1,8 @@
 # Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "Invoice no note from sale",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-account",
