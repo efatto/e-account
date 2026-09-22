0ae9c859aa90cc6b2157bd360681a2bababdbc54
Sergio Corato <sergiocorato@gmail.com>
2026-09-22 09:31:42 +0200

    [MIG] res_partner_email_shipping: Migration to 18.0

--- res_partner_email_shipping/__manifest__.py
+++ res_partner_email_shipping/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Additional mail shipping for partner",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "A simple mail used for shipping.",
--- res_partner_email_shipping/models/partner.py
+++ res_partner_email_shipping/models/partner.py
@@ -5,7 +5,6 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     email_shipping = fields.Char(
-        string="Email Shipping",
         help="Email address that can be used in email shipping templates",
     )
     email_shipping_template_id = fields.Many2one(
