f71739510d1ad8c177bd4015cc2c76e94bd9c5fb
Sergio Corato <sergiocorato@gmail.com>
2026-09-23 10:46:33 +0200

    [MIG] sale_order_view_usability: Migration to 18.0

--- sale_order_view_usability/__manifest__.py
+++ sale_order_view_usability/__manifest__.py
@@ -2,12 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale order view usability",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
-    "summary": "Extend sale order tree view adding origin and moving state after the "
-    "partner.  sale order ref in form view, "
-    "show origin field to base user.",
     "website": "https://github.com/efatto/e-account",
     "license": "AGPL-3",
     "depends": [
---
+++ sale_order_view_usability/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module extends sale order list view adding origin and moving state after the partner, put sale order ref in form view and show origin field to base user.
