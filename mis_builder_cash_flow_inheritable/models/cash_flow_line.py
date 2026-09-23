f297a3c7fe308da0613acff55c829bfe1ef2ad73
Sergio Corato <sergiocorato@gmail.com>
2026-09-23 17:17:23 +0200

    [IMP] move method _get_account in mis_builder_cash_flow_inheritable to be reused

--- mis_builder_cash_flow_inheritable/models/cash_flow_line.py
+++ mis_builder_cash_flow_inheritable/models/cash_flow_line.py
@@ -11,3 +11,34 @@ class CashFlowForecastLine(models.Model):
     res_model_id = fields.Many2one(comodel_name="ir.model", index=True)
     res_model = fields.Char(related="res_model_id.model", store=True)
     currency_id = fields.Many2one(comodel_name="res.currency")
+
+
+class CashFlowMixin(models.AbstractModel):
+    _name = "mis.cash_flow.mixin"
+    _description = "Cash Flow Mixin"
+
+    company_id = fields.Many2one(comodel_name="res.company")
+
+    def _get_account(self, account_ref):
+        """
+        account_ref: str
+        Return account id for given account_ref.
+        """
+        chart_template = self.with_context(
+            allowed_company_ids=self.company_id.root_id.ids
+        ).env["account.chart.template"]
+        outstanding_account_id = (
+            chart_template.ref(account_ref, raise_if_not_found=False)
+            or self.env["account.account"].search(
+                [
+                    (
+                        "account_type",
+                        "=",
+                        "asset_cash",
+                    ),
+                    ("company_ids", "in", self.company_id.id),
+                ],
+                limit=1,
+            )
+        ).id
+        return outstanding_account_id
