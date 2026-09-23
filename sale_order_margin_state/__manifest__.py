47483a1f65e1abdf6fd49588aa01d013ae72bcf3
Sergio Corato <sergiocorato@gmail.com>
2026-09-23 10:43:35 +0200

    [MIG] sale_order_margin_state: Migration to 18.0

--- sale_order_margin_state/__manifest__.py
+++ sale_order_margin_state/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale order margin state",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Add state of margin in sale order line.",
